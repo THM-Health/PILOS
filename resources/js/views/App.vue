f7725b81fac9d7945de0c2df1b0fe153c6273669
Dennis Stumm <dstumm@yandex.ru>
2020-06-17 11:52:11 +0200

    Add loading overlay in app view

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,38 +1,49 @@
 <template>
-    <div>
-        <b-navbar class="mainnav" toggleable="lg" type="light" variant="white">
-            <b-container>
-                <b-navbar-brand :to="{ name: 'home' }">
-                    <img style="height: 2rem;" src="https://11.pilos-thm.de/logo/default/THMPilos.svg">
-                </b-navbar-brand>
+  <div>
+    <b-overlay :show="loadingCounter > 0">
+      <template v-slot:overlay>
+        <div class="text-center">
+          <b-spinner variant="dark" label="Bitte warten..."></b-spinner>
+          <p>Bitte warten...</p>
+        </div>
+      </template>
 
-                <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-navbar class="mainnav" toggleable="lg" type="light" variant="white">
+        <b-container>
+          <h1>
+            <b-navbar-brand :to="{ name: 'home' }">
+              <img style="height: 2rem;" src="https://11.pilos-thm.de/logo/default/THMPilos.svg" alt="Logo">
+            </b-navbar-brand>
+          </h1>
 
-                <b-collapse id="nav-collapse" is-nav>
-                    <b-navbar-nav>
-                        <b-nav-item :to="{ name: 'rooms.index' }" v-if='isAuthenticated'>Rooms</b-nav-item>
-                    </b-navbar-nav>
+          <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-                    <!-- Right aligned nav items -->
-                    <b-navbar-nav class="ml-auto">
-                      <b-nav-item :to="{ name: 'login' }" v-if='!isAuthenticated' right>Login</b-nav-item>
-                        <b-nav-item-dropdown right v-if='isAuthenticated'>
-                            <!-- Using 'button-content' slot -->
-                            <template v-slot:button-content>
-                                {{currentUser.firstname}} {{currentUser.lastname}}
-                            </template>
-                            <b-dropdown-item href="#">Profile</b-dropdown-item>
-                            <b-dropdown-item @click="logout">Sign Out</b-dropdown-item>
-                        </b-nav-item-dropdown>
-                    </b-navbar-nav>
-                </b-collapse>
-            </b-container>
-        </b-navbar>
+          <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav>
+              <b-nav-item :to="{ name: 'rooms.index' }" v-if='isAuthenticated'>Rooms</b-nav-item>
+            </b-navbar-nav>
 
-        <main>
-            <router-view></router-view>
-        </main>
-    </div>
+            <!-- Right aligned nav items -->
+            <b-navbar-nav class="ml-auto">
+              <b-nav-item :to="{ name: 'login' }" v-if='!isAuthenticated' right>Login</b-nav-item>
+              <b-nav-item-dropdown right v-if='isAuthenticated'>
+                <!-- Using 'button-content' slot -->
+                <template v-slot:button-content>
+                  {{currentUser.firstname}} {{currentUser.lastname}}
+                </template>
+                <b-dropdown-item href="#">Profile</b-dropdown-item>
+                <b-dropdown-item @click="logout">Sign Out</b-dropdown-item>
+              </b-nav-item-dropdown>
+            </b-navbar-nav>
+          </b-collapse>
+        </b-container>
+      </b-navbar>
+
+      <main>
+        <router-view></router-view>
+      </main>
+    </b-overlay>
+  </div>
 </template>
 
 <script>
@@ -41,7 +52,8 @@ import { mapState, mapGetters } from 'vuex'
 export default {
   computed: {
     ...mapState({
-      currentUser: state => state.session.currentUser
+      currentUser: state => state.session.currentUser,
+      loadingCounter: state => state.loadingCounter
     }),
     ...mapGetters({
       isAuthenticated: 'session/isAuthenticated'
@@ -49,7 +61,6 @@ export default {
   },
   methods: {
     async logout () {
-      // TODO: Loading indicator
       await this.$store.dispatch('session/logout')
       // TODO: Message for successful logout
       this.$router.push({ name: 'home' })
@@ -59,7 +70,7 @@ export default {
 </script>
 
 <style scoped>
-  .mainnav {
-    border-bottom: 1px solid rgba(0,40,100,0.12);
-  }
+.mainnav {
+  border-bottom: 1px solid rgba(0, 40, 100, 0.12);
+}
 </style>
