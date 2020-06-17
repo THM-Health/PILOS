1d6b35b4def1c34d91fa386e0d536ca0c314d6d0
Dennis Stumm <dstumm@yandex.ru>
2020-06-17 11:18:53 +0200

    Add logo to project resources

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,6 +14,7 @@ const mix = require('laravel-mix')
 mix.js('resources/js/app.js', 'public/js')
   .sass('resources/sass/app.scss', 'public/css')
   .sourceMaps(false)
+  .copy('resources/images', 'public/images')
   .extract()
 
 if (mix.inProduction()) {
