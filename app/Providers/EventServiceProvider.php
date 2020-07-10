33a723047436877fd23f4deed0375aa88b5449bd
Samuel Weirich <samuel.weirich@gmx.de>
2020-07-10 09:34:55 +0200

    Revert "Add event listener to get protected ldap attributes of user"
    This reverts commit a9429a871f277c0354d04897028c9ca722507f90.

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,12 +2,10 @@
 
 namespace App\Providers;
 
-use App\Listeners\SetProtectedLdapAttributes;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
-use LdapRecord\Laravel\Events\Authenticated;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -20,9 +18,6 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
-        Authenticated::class => [
-            SetProtectedLdapAttributes::class
-        ]
     ];
 
     /**
