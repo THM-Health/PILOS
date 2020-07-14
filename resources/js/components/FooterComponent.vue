d390209fdd9c2f2e206cfa88c11cd1878fd13740
rhrt04 <roby.hartono@ges.thm.de>
2020-07-14 19:50:45 +0200

    Changed footer to use bootstrap vue classes

--- resources/js/components/FooterComponent.vue
+++ resources/js/components/FooterComponent.vue
@@ -1,12 +1,12 @@
 <template>
   <footer class="footer">
-    <b-container class="d-flex justify-content-center border-top bg-white" fluid>
-      <b-row>
+    <b-container class="border-top bg-white" fluid>
+      <b-row align-h="center" align-v="center">
         <a :href="$t('app.footer.legalNoticeUrl')">
           <small>{{$t('app.footer.legalNotice')}}</small>
         </a>
 
-        <p class="mr-2 ml-2">{{$t('app.footer.separator')}}</p>
+        <a class="mr-2 ml-2"><small>{{$t('app.footer.separator')}}</small></a>
 
         <a :href="$t('app.footer.privacyPolicyUrl')">
           <small>{{$t('app.footer.privacyPolicy')}}</small>
