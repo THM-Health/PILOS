858d16d480ca16f2b156dd767893c95d282351f1
rhrt04 <roby.hartono@ges.thm.de>
2020-07-16 11:37:28 +0200

    Added localization files for admin sites and components

--- resources/js/lang/de.js
+++ resources/js/lang/de.js
@@ -1,9 +1,11 @@
+import admins from './de/admins'
 import app from './de/app'
 import auth from './de/auth'
 import home from './de/home'
 import rooms from './de/rooms'
 
 export default {
+  admins,
   app,
   auth,
   home,
--- resources/js/lang/en.js
+++ resources/js/lang/en.js
@@ -1,9 +1,11 @@
+import admins from './de/admins'
 import app from './en/app'
 import auth from './en/auth'
 import home from './en/home'
 import rooms from './en/rooms'
 
 export default {
+  admins,
   app,
   auth,
   home,
