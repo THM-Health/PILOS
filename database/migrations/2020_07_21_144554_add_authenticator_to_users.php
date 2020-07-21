2440dd95284fec973103139fc88ab447c47ba6f8
Dennis Stumm <dstumm@yandex.ru>
2020-07-21 18:04:37 +0200

    Add default for authenticator column

--- database/migrations/2020_07_21_144554_add_authenticator_to_users.php
+++ database/migrations/2020_07_21_144554_add_authenticator_to_users.php
@@ -14,7 +14,7 @@ class AddAuthenticatorToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('authenticator');
+            $table->string('authenticator')->default('users');
         });
     }
 
