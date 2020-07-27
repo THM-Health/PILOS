6b264aaebad4d27a3223d026758155e67c5d7e03
Samuel Weirich <samuel.weirich@gmx.de>
2020-07-27 09:23:02 +0200

    Cleanup

--- resources/js/components/Room/RoomComponent.vue
+++ resources/js/components/Room/RoomComponent.vue
@@ -1,24 +1,20 @@
 <template>
   <div>
-    <b-card no-body bg-variant="white" class="roomcard" v-on:click="say('hi')">
+    <b-card no-body bg-variant="white" class="roomcard" @click="$router.push({ name: 'rooms.view', params: { id: id }})">
       <b-card-body class="p-3">
       <b-media>
         <template v-slot:aside>
           <div v-if="type" class="roomicon" :style="{ 'background-color': type.color}">{{type.short}}</div>
         </template>
-
         <h4 class="mt-2 ">{{name}}</h4>
       </b-media>
       </b-card-body>
       <template v-slot:footer v-if="shared">
         <small><i class="fas fa-share"></i> {{ $t('sharedBy', { name: sharedBy }) }}</small>
       </template>
-
     </b-card>
-
   </div>
 </template>
-
 <script>
 export default {
   props: {
@@ -31,22 +27,8 @@ export default {
     type: Object,
     sharedBy: String
   },
-
-  data () {
-    return {
-
-    }
-  },
-
-  methods: {
-    say: function (message) {
-      this.$router.push({ name: 'rooms.view', params: { id: this.id } })
-    }
-  }
-
 }
 </script>
-
 <style scoped>
 .roomcard{
   pointer-events: auto;
