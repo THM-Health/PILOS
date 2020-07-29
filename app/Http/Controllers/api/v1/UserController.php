a94a5643b4f4b589fc9e5030f371bde0e567032c
Samuel Weirich <samuel.weirich@gmx.de>
2020-07-29 12:43:19 +0200

    change from custom user search to scopes

--- app/Http/Controllers/api/v1/UserController.php
+++ app/Http/Controllers/api/v1/UserController.php
@@ -16,23 +16,6 @@ class UserController extends Controller
      */
     public function search(Request $request)
     {
-        // Remove multiple whitespaces
-        $query = preg_replace('/\s\s+/', ' ', $request->get('query'));
-        // Split each word in a separate query
-        $query = explode(' ', $query);
-        // define columns in which the query should exists
-        $search_columns = ['firstname','lastname'];
-        // find in users, where each query must exist in at least one of the defined columns
-        $users = User::where(function ($a) use ($query,$search_columns) {
-            foreach ($query as $value) {
-                $a->where(function ($b) use ($value,$search_columns) {
-                    foreach ($search_columns as $search_column) {
-                        $b->orWhere($search_column, 'like', "%{$value}%");
-                    }
-                });
-            }
-        })->get();
-        // return json with the results
-        return \App\Http\Resources\User::collection($users);
+        return \App\Http\Resources\User::collection(User::withName( $request->get('query'))->limit(10)->get());
     }
 }
