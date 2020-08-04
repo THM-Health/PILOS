67f43e9d9a6fa09ca3c58c55d72cd72c06954971
Dennis Stumm <dstumm@yandex.ru>
2020-08-04 20:11:21 +0200

    Add migrations for permissions and roles tables

---
+++ database/migrations/2020_08_04_163252_create_permissions_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePermissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permissions', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->timestamps();
+        });
+
+        Schema::create('role_permission', function (Blueprint $table) {
+            $table->foreignId('permission_id')
+                ->constrained()
+                ->onDelete('cascade');
+
+            $table->foreignId('role_id')
+                ->constrained()
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('permissions');
+        Schema::dropIfExists('role_permission');
+    }
+}
