bd78605c4c4258d1277d393f7232c4efc5cf4ebe
Samuel Weirich <samuel.weirich@gmx.de>
2020-08-31 11:22:32 +0200

    add tests for room limit reached event and reload

--- tests/Frontend/components/Room/RoomList.spec.js
+++ tests/Frontend/components/Room/RoomList.spec.js
@@ -6,10 +6,25 @@ import moxios from 'moxios';
 import RoomComponent from '../../../../resources/js/components/Room/RoomComponent';
 import sinon from 'sinon';
 import VueRouter from 'vue-router';
+import NewRoomComponent from '../../../../resources/js/components/Room/NewRoomComponent';
 
 const localVue = createLocalVue();
 localVue.use(BootstrapVue);
 localVue.use(IconsPlugin);
+
+function overrideStub (url, response) {
+  const l = moxios.stubs.count();
+  for (let i = 0; i < l; i++) {
+    const stub = moxios.stubs.at(i);
+    if (stub.url === url) {
+      const oldResponse = stub.response;
+      const restoreFunc = () => { stub.response = oldResponse; };
+
+      stub.response = response;
+      return restoreFunc;
+    }
+  }
+}
 
 describe('RoomList', function () {
   beforeEach(function () {
@@ -63,7 +78,7 @@ describe('RoomList', function () {
     ]
   };
 
-  const exampleUser = { id: 1, firstname: 'John', lastname: 'Doe', locale: 'de', permissions: [], modelName: 'User' };
+  const exampleUser = { id: 1, firstname: 'John', lastname: 'Doe', locale: 'de', permissions: ['rooms.create'], modelName: 'User' };
 
   it('check list of rooms', function (done) {
     moxios.stubRequest('/api/v1/rooms', {
@@ -139,6 +154,103 @@ describe('RoomList', function () {
       sinon.assert.calledOnce(spy);
       sinon.assert.calledWith(spy, { name: 'rooms.view', params: { id: exampleRoomListEntry.id } });
       done();
+    });
+  });
+
+  it('test reload function and room limit reach event', function (done) {
+    moxios.stubRequest('/api/v1/rooms', {
+      status: 200,
+      response: {
+        data: {
+          myRooms: [
+            {
+              id: 'abc-def-123',
+              name: 'Meeting One',
+              owner: 'John Doe',
+              type: {
+                id: 2,
+                short: 'ME',
+                description: 'Meeting',
+                color: '#4a5c66',
+                default: false
+              }
+            }
+          ],
+          sharedRooms: []
+        }
+      }
+    });
+
+    moxios.stubRequest('/api/v1/application', {
+      status: 200,
+      response: { data: { settings: { room_limit: -1 }, user: exampleUser } }
+    });
+
+    const view = mount(RoomList, {
+      localVue,
+      mocks: {
+        $t: (key) => key
+      },
+      store
+    });
+
+    store.dispatch('initialize', {});
+
+    RoomList.beforeRouteEnter.call(view.vm, undefined, undefined, async next => {
+      next(view.vm);
+      await view.vm.$nextTick();
+      // find current amount of rooms
+      const rooms = view.findAllComponents(RoomComponent);
+      expect(rooms.length).toBe(1);
+
+      // change response and fire event
+      overrideStub('/api/v1/rooms', {
+        status: 200,
+        response: {
+          data: {
+            myRooms: [
+              {
+                id: 'abc-def-123',
+                name: 'Meeting One',
+                owner: 'John Doe',
+                type: {
+                  id: 2,
+                  short: 'ME',
+                  description: 'Meeting',
+                  color: '#4a5c66',
+                  default: false
+                }
+              },
+              {
+                id: 'abc-def-456',
+                name: 'Meeting Two',
+                owner: 'John Doe',
+                type: {
+                  id: 2,
+                  short: 'ME',
+                  description: 'Meeting',
+                  color: '#4a5c66',
+                  default: false
+                }
+              }
+            ],
+            sharedRooms: []
+          }
+        }
+      });
+
+      // find new room component and fire event
+      const newRoomComponent = view.findComponent(NewRoomComponent);
+      expect(newRoomComponent.exists()).toBeTruthy();
+      newRoomComponent.vm.$emit('limitReached');
+
+      moxios.wait(function () {
+        view.vm.$nextTick();
+        // check if now two rooms are displayed
+        const rooms = view.findAllComponents(RoomComponent);
+        expect(rooms.length).toBe(2);
+        done();
+      });
     });
   });
 });
