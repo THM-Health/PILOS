a612843f8b26272dd5a4692a7a4cc059f68ac896
Dennis Stumm <dstumm@yandex.ru>
2020-09-02 13:59:49 +0200

    Cherry pick missing changes

---
+++ app/Http/Resources/Role.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Role extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'default' => $this->default,
+            'updated_at' => $this->updated_at,
+            'permissions' => Permission::collection($this->whenLoaded('permissions')),
+        ];
+    }
+}
