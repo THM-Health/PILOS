9311e92dda7745f860e2f156f697b1b155000725
Dennis Stumm <dstumm@yandex.ru>
2020-09-15 17:26:42 +0200

    Fix cs

--- tests/Frontend/views/settings/roles/Index.spec.js
+++ tests/Frontend/views/settings/roles/Index.spec.js
@@ -2,7 +2,7 @@ import Index from '../../../../../resources/js/views/settings/roles/Index';
 import { createLocalVue, mount } from '@vue/test-utils';
 import PermissionService from '../../../../../resources/js/services/PermissionService';
 import moxios from 'moxios';
-import BootstrapVue, { IconsPlugin, BTr, BTbody, BSpinner, BPagination } from 'bootstrap-vue';
+import BootstrapVue, { IconsPlugin, BTr, BTbody } from 'bootstrap-vue';
 
 const localVue = createLocalVue();
 localVue.use(BootstrapVue);
