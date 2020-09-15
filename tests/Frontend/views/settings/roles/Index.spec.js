b054d92be7d93413b4713a095fadfb1a9cdaab82
Dennis Stumm <dstumm@yandex.ru>
2020-09-15 17:18:24 +0200

    Add test stubs and first frontend test

---
+++ tests/Frontend/views/settings/roles/Index.spec.js
@@ -0,0 +1,115 @@
+import Index from '../../../../../resources/js/views/settings/roles/Index';
+import { createLocalVue, mount } from '@vue/test-utils';
+import PermissionService from '../../../../../resources/js/services/PermissionService';
+import moxios from 'moxios';
+import BootstrapVue, { IconsPlugin, BTr, BTbody, BSpinner, BPagination } from 'bootstrap-vue';
+
+const localVue = createLocalVue();
+localVue.use(BootstrapVue);
+localVue.use(IconsPlugin);
+
+describe('RolesIndex', function () {
+  beforeEach(function () {
+    moxios.install();
+  });
+
+  afterEach(function () {
+    moxios.uninstall();
+  });
+
+  it('list of roles with pagination gets displayed', function (done) {
+    const oldUser = PermissionService.currentUser;
+
+    PermissionService.setCurrentUser({ permissions: ['roles.viewAny', 'settings.view'] });
+
+    const view = mount(Index, {
+      localVue,
+      mocks: {
+        $t: key => key,
+        $te: key => key === 'app.roles.admin'
+      }
+    });
+
+    moxios.wait(function () {
+      expect(view.findComponent(BTbody).findComponent(BTr).html()).toContain('b-table-busy-slot');
+
+      const request = moxios.requests.mostRecent();
+      request.respondWith({
+        status: 200,
+        response: {
+          data: [{
+            id: '1',
+            name: 'Test',
+            default: false,
+            modelName: 'Role'
+          }],
+          meta: {
+            per_page: 1,
+            current_page: 1,
+            total: 1
+          }
+        }
+      }).then(() => {
+        return view.vm.$nextTick();
+      }).then(() => {
+        let html = view.findComponent(BTbody).findComponent(BTr).html();
+        expect(html).toContain('Test');
+        expect(html).toContain('app.false');
+        expect(html).toContain('1');
+
+        view.vm.$root.$emit('bv::refresh::table', 'roles-table');
+
+        moxios.wait(function () {
+          expect(view.findComponent(BTbody).findComponent(BTr).html()).toContain('b-table-busy-slot');
+
+          const request = moxios.requests.mostRecent();
+          request.respondWith({
+            status: 200,
+            response: {
+              data: [{
+                id: '2',
+                name: 'admin',
+                default: true,
+                modelName: 'Role'
+              }],
+              meta: {
+                per_page: 1,
+                current_page: 1,
+                total: 1
+              }
+            }
+          }).then(() => {
+            html = view.findComponent(BTbody).findComponent(BTr).html();
+
+            expect(html).toContain('app.roles.admin');
+            expect(html).toContain('app.true');
+            expect(html).toContain('2');
+
+            PermissionService.setCurrentUser(oldUser);
+            done();
+          });
+        });
+      });
+    });
+  });
+
+  it('update and delete buttons only shown if user has the permission and the role is not system default', function () {
+
+  });
+
+  it('error handler gets called if an error occurs during loading of data', function () {
+
+  });
+
+  it('not system roles can be deleted', function () {
+
+  });
+
+  it('property gets cleared correctly if deletion gets aborted', function () {
+
+  });
+
+  it('new role button is displayed if the user has the corresponding permissions', function () {
+
+  });
+});
---
+++ tests/Frontend/views/settings/roles/View.spec.js
@@ -0,0 +1,50 @@
+
+describe('RolesView', function () {
+  it('role name in title gets translated for detail view', function () {
+
+  });
+
+  it('role name in title gets translated for update view', function () {
+
+  });
+
+  it('input fields are disabled if the role is displayed in view mode', function () {
+
+  });
+
+  it('data gets loaded for update view of a role', function () {
+
+  });
+
+  it('loading indicator gets displayed during load of data', function () {
+
+  });
+
+  it('error handler gets called if an error occurs during load of data', function () {
+
+  });
+
+  it('back button causes a back navigation without persistence', function () {
+
+  });
+
+  it('request with updates get send during saving the role', function () {
+
+  });
+
+  it('validation errors gets shown for the appropriate fields', function () {
+
+  });
+
+  it('modal gets shown for stale errors and a overwrite can be forced', function () {
+
+  });
+
+  it('modal gets shown for stale errors and the new model can be applied to current form', function () {
+
+  });
+
+  it('permissions shown in pages in the multiselect', function () {
+
+  });
+});
