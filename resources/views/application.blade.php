708101aebd27dc9e8a58638a302f66137986a1e3
Samuel Weirich <samuel.weirich@gmx.de>
2020-10-01 17:57:35 +0200

    Add versioning for css and js code

--- resources/views/application.blade.php
+++ resources/views/application.blade.php
@@ -10,11 +10,11 @@
     <title>{{ config('app.name', 'PILOS') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ mix('js/app.js') }}" defer></script>
 
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id='app'>
