4ab01ff986bae34390aab6c258dbb080271c2a28
Dennis Stumm <dstumm@yandex.ru>
2020-12-02 17:09:08 +0100

    Add command to create an admin user

---
+++ tests/Unit/CreateAdminUser.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Role;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CreateAdminUser extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    /**
+     * @see TestCase::setUp()
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        config([
+            'app.available_locales' => ['de', 'en'],
+            'app.fallback_locale'   => 'ru',
+            'app.locale'            => 'en'
+        ]);
+    }
+
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testInvalidInputs()
+    {
+        factory(Role::class)->create(['name' => 'admin']);
+
+        $this->artisan('users:create:admin')
+            ->expectsOutput('Creating an new admin user, please notify your inputs.')
+            ->expectsQuestion('Firstname', str_repeat('a', 256))
+            ->expectsQuestion('Lastname', str_repeat('a', 256))
+            ->expectsQuestion('E-Mail', str_repeat('a', 256))
+            ->expectsQuestion('Locale (possible values: ' . join(',', config('app.available_locales')) . ')', str_repeat('a', 256))
+            ->expectsQuestion('Password', 'Test')
+            ->expectsQuestion('Password Confirmation', 'Test1234')
+            ->expectsOutput('Something went wrong, please see the error messages below for more information.')
+            ->assertExitCode(1);
+    }
+
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testMissingRole()
+    {
+        $this->artisan('users:create:admin')
+            ->expectsOutput('The admin role does not exist. Please seed the database and then retry!')
+            ->assertExitCode(1);
+    }
+
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testValidInputs()
+    {
+        factory(Role::class)->create(['name' => 'admin']);
+
+        $this->artisan('users:create:admin')
+            ->expectsOutput('Creating an new admin user, please notify your inputs.')
+            ->expectsQuestion('Firstname', $this->faker->firstName)
+            ->expectsQuestion('Lastname', $this->faker->lastName)
+            ->expectsQuestion('E-Mail', $this->faker->email)
+            ->expectsQuestion('Locale (possible values: ' . join(',', config('app.available_locales')) . ')', config('app.available_locales')[0])
+            ->expectsQuestion('Password', 'Test_1234')
+            ->expectsQuestion('Password Confirmation', 'Test_1234')
+            ->expectsOutput('New admin user created successfully.')
+            ->assertExitCode(0);
+
+        $this->assertDatabaseCount('users', 1);
+    }
+}
