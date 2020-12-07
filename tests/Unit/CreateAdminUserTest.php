7311135e4de51c41de632c6d4cd3620c61179f8e
Dennis Stumm <dstumm@yandex.ru>
2020-12-07 10:21:08 +0100

    Add suffix to artisan command test
    Since only tests with the `Test.php` suffix gets executed by phpunit

--- tests/Unit/CreateAdminUserTest.php
+++ tests/Unit/CreateAdminUserTest.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class CreateAdminUser extends TestCase
+class CreateAdminUserTest extends TestCase
 {
     use RefreshDatabase, WithFaker;
 
