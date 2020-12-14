a3660304b55ac9f6695e3f1385676ad69fe2dc08
Samuel Weirich <samuel.weirich@gmx.de>
2020-12-14 15:38:59 +0100

    add routes

--- routes/api.php
+++ routes/api.php
@@ -80,6 +80,8 @@ Route::prefix('v1')->namespace('api\v1')->name('api.v1.')->group(function () {
         Route::apiResource('users', 'UserController');
 
         Route::get('roomTypes', 'RoomTypeController@index')->name('roomTypes.index');
+        Route::apiResource('servers', 'ServerController');
+
     });
 
     Route::middleware('can:view,room')->group(function () {
