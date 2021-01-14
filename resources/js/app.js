e1629024d1a9b675012c00b969f5373c3cb7002a
Samuel Weirich <samuel.weirich@gmx.de>
2021-01-14 13:49:33 +0100

    Add tests

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,6 @@ import router from './router';
 import i18n from './i18n';
 import FlashMessage from '@smartweb/vue-flash-message';
 import Clipboard from 'v-clipboard';
-import env from './env';
 
 Vue.use(Clipboard);
 // Install BootstrapVue
@@ -24,46 +23,6 @@ if (process.env.NODE_ENV === 'development') {
   const VueAxe = require('vue-axe').default;
   Vue.use(VueAxe);
 }
-
-/**
- * Global error handler for unhandled errors that can occur in the application.
- *
- * Make sure that you catch possible errors caused by requests to the server (e.g. validation errors)
- * in the appropriate place in the application. This handler is only for the last instance if there
- * is something going on, that should be normally.
- *
- * @param error The occurred error
- * @param vm The vue instance
- * @param info Some additional error information
- */
-Vue.config.errorHandler = function (error, vm, info) {
-  const responseStatus = error.response !== undefined ? error.response.status : undefined;
-  const errorMessage = error.response && error.response.data ? error.response.data.message : undefined;
-
-  if (responseStatus === env.HTTP_UNAUTHORIZED) { // 401 => unauthorized, redirect and show error messages as flash!
-    if (vm.$store.getters['session/isAuthenticated']) {
-      vm.flashMessage.info(vm.$t('app.flash.unauthenticated'));
-      vm.$store.commit('setCurrentUser', { currentUser: null, emit: false });
-      vm.$router.replace({ name: 'login', query: { redirect: vm.$router.currentRoute.path } });
-    }
-  } else if (responseStatus === env.HTTP_FORBIDDEN && errorMessage === 'This action is unauthorized.') { // 403 => unauthorized, show error messages as flash!
-    vm.flashMessage.error(vm.$t('app.flash.unauthorized'));
-  } else if (responseStatus === env.HTTP_GUESTS_ONLY) { // 420 => only for guests, redirect to home route
-    vm.flashMessage.info(vm.$t('app.flash.guestsOnly'));
-    vm.$router.replace({ name: 'home' });
-  } else if (responseStatus === env.HTTP_PAYLOAD_TOO_LARGE) { // 413 => payload to large
-    vm.flashMessage.error(vm.$t('app.flash.tooLarge'));
-  } else if (responseStatus !== undefined) { // Another error on server
-    vm.flashMessage.error({
-      message: errorMessage ? vm.$t('app.flash.serverError.message', { message: errorMessage }) : vm.$t('app.flash.serverError.emptyMessage'),
-      title: vm.$t('app.flash.serverError.title', { statusCode: responseStatus }),
-      contentClass: 'flash_small_title flex-column-reverse d-flex'
-    });
-  } else {
-    vm.flashMessage.error(vm.$t('app.flash.clientError'));
-    console.error(`Error: ${error.toString()}\nInfo: ${info}`);
-  }
-};
 
 export default new Vue({
   el: '#app',
--- tests/Frontend/api/base.spec.js
+++ tests/Frontend/api/base.spec.js
@@ -1,6 +1,7 @@
 import Base from '../../../resources/js/api/base';
 import moxios from 'moxios';
 import sinon from 'sinon';
+import VueRouter from 'vue-router';
 
 describe('base', function () {
   beforeEach(function () {
@@ -61,14 +62,49 @@ describe('base', function () {
     });
   });
 
-  it('`error` calls the vue error handler with the passed params', function () {
-    const args = ['a', 'b', 'c'];
-    let passedArgs = [];
+  it('base error handling 401', function () {
+    const flashMessageSpy = sinon.spy();
+    const flashMessage = {
+      info (param) {
+        flashMessageSpy(param);
+      }
+    };
 
-    Base.__Rewire__('Vue', { config: { errorHandler: (...args) => { passedArgs = args; } } });
-    Base.error(...args);
-    expect(passedArgs).toEqual(args);
-    Base.__ResetDependency__('Vue');
+    const routerSpy = sinon.spy();
+    const router = new VueRouter();
+    router.replace = routerSpy;
+    const sandbox = sinon.createSandbox();
+    sandbox.replaceGetter(router, 'currentRoute', function () {
+      return { path: '/test' };
+    });
+
+    const storeCommitSpy = sinon.spy();
+    const store = {
+      getters: {
+        'session/isAuthenticated': true
+      },
+      commit: storeCommitSpy
+    };
+
+    const vm = {
+      $store: store,
+      $router: router,
+      flashMessage: flashMessage,
+      $t: (key) => key
+
+    };
+
+    const error = { response: { data: { message: 'Unauthenticated.' }, status: 401, statusText: 'Unauthorized' }, message: 'Request failed with status code 401' };
+    Base.error(error, vm, error.message);
+
+    sinon.assert.calledOnce(routerSpy);
+    sinon.assert.calledWith(routerSpy, { name: 'login', query: { redirect: '/test' } });
+
+    sinon.assert.calledOnce(flashMessageSpy);
+    sinon.assert.calledWith(flashMessageSpy, 'app.flash.unauthenticated');
+
+    sinon.assert.calledOnce(storeCommitSpy);
+    sinon.assert.calledWith(storeCommitSpy, 'setCurrentUser', { currentUser: null, emit: false });
   });
 
   it('`getCsrfCookie` calls the route for getting a csrf cookie', function (done) {
