6bce3b94cc899e34b4205c16781b7f79f1a04f12
Samuel Weirich <samuel.weirich@gmx.de>
2021-01-25 14:51:25 +0100

    Backend test

---
+++ tests/Feature/api/v1/ServerPoolTest.php
@@ -0,0 +1,356 @@
+<?php
+
+namespace Tests\Feature\api\v1;
+
+use App\Enums\CustomStatusCodes;
+use App\Permission;
+use App\Role;
+use App\RoomType;
+use App\Server;
+use App\ServerPool;
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+/**
+ * General server pool api feature tests
+ * @package Tests\Feature\api\v1\Server
+ */
+class ServerPoolTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    protected $user;
+
+    /**
+     * Setup resources for all tests
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->user = factory(User::class)->create();
+    }
+
+    /**
+     * Test to get a list of all server pools
+     */
+    public function testIndex()
+    {
+        $page_size = 5;
+        setting(['pagination_page_size' => $page_size]);
+        ServerPool::truncate();
+        $serverPools  = factory(ServerPool::class, 9)->create();
+        $serverPool1  = factory(ServerPool::class)->create(['name'=>'testPool']);
+
+        // Test guests
+        $this->getJson(route('api.v1.serverPools.index'))
+            ->assertUnauthorized();
+
+        $this->actingAs($this->user)->getJson(route('api.v1.serverPools.index'))
+            ->assertForbidden();
+
+        // Authenticated user with permission
+        $role       = factory(Role::class)->create(['default' => true]);
+        $permission = Permission::firstOrCreate([ 'name' => 'serverPools.viewAny' ]);
+        $role->permissions()->attach($permission->id);
+        $role->users()->attach($this->user->id);
+
+        $this->actingAs($this->user)->getJson(route('api.v1.serverPools.index'))
+            ->assertSuccessful()
+            ->assertJsonCount($page_size, 'data')
+            ->assertJsonFragment(['id' => $serverPools[0]->id])
+            ->assertJsonFragment(['id' => $serverPools[4]->id])
+            ->assertJsonFragment(['per_page' => $page_size])
+            ->assertJsonFragment(['total' => 10])
+            ->assertJsonStructure([
+                'meta',
+                'links',
+                'data' => [
+                    '*' => [
+                        'id',
+                        'name',
+                        'description',
+                        'server_count',
+                        'updated_at',
+                        'model_name'
+                    ]
+                ]
+            ]);
+
+        // Pagination
+        $this->getJson(route('api.v1.serverPools.index') . '?page=2')
+            ->assertSuccessful()
+            ->assertJsonCount($page_size, 'data')
+            ->assertJsonFragment(['id' => $serverPools[5]->id]);
+
+        // Filtering by name
+        $this->getJson(route('api.v1.serverPools.index') . '?name=testPool')
+            ->assertSuccessful()
+            ->assertJsonCount(1, 'data')
+            ->assertJsonFragment(['id' => $serverPool1->id]);
+
+        // Sorting status asc
+        $this->getJson(route('api.v1.serverPools.index') . '?sort_by=name&sort_direction=asc')
+            ->assertSuccessful()
+            ->assertJsonCount($page_size, 'data')
+            ->assertJsonFragment(['id' => ServerPool::orderBy('name')->first()->id])
+            ->assertJsonMissing(['id' => ServerPool::orderByDesc('name')->first()->id]);
+
+        // Sorting status desc
+        $this->getJson(route('api.v1.serverPools.index') . '?sort_by=name&sort_direction=desc')
+            ->assertSuccessful()
+            ->assertJsonCount($page_size, 'data')
+            ->assertJsonFragment(['id' => ServerPool::orderByDesc('name')->first()->id])
+            ->assertJsonMissing(['id' => ServerPool::orderBy('name')->first()->id]);
+    }
+
+    /**
+     * Test to view single server pool
+     */
+    public function testShow()
+    {
+        $serverPool = factory(ServerPool::class)->create();
+        $server     = factory(Server::class, 5)->create();
+        $serverPool->servers()->sync($server);
+
+        // Test guests
+        $this->getJson(route('api.v1.serverPools.show', ['serverPool' => $serverPool->id]))
+            ->assertUnauthorized();
+
+        // Test logged in users
+        $this->actingAs($this->user)->getJson(route('api.v1.serverPools.show', ['serverPool' => $serverPool->id]))
+            ->assertForbidden();
+
+        // Authorize user
+        $role       = factory(Role::class)->create();
+        $permission = factory(Permission::class)->create(['name' => 'serverPools.view']);
+        $role->permissions()->attach($permission);
+        $this->user->roles()->attach($role);
+
+        // Test with authorized user
+        $this->actingAs($this->user)->getJson(route('api.v1.serverPools.show', ['serverPool' => $serverPool->id]))
+            ->assertSuccessful()
+            ->assertJsonFragment([
+                'id'                      => $serverPool->id,
+                'name'                    => $serverPool->name,
+                'description'             => $serverPool->description,
+                'server_count'            => $serverPool->servers()->count(),
+                'updated_at'              => $serverPool->updated_at,
+                'model_name'              => $serverPool->model_name
+                ])
+            ->assertJsonCount(5, 'data.servers');
+
+        // Test deleted
+        $serverPool->delete();
+        $this->actingAs($this->user)->getJson(route('api.v1.serverPools.show', ['serverPool' => $serverPool->id]))
+            ->assertNotFound();
+    }
+
+    /**
+     * Test to create new server pool
+     */
+    public function testCreate()
+    {
+        $serverPool = factory(ServerPool::class)->make();
+        $data       = [
+            'name'        => $serverPool->name,
+            'description' => $serverPool->description,
+        ];
+
+        // Test guests
+        $this->postJson(route('api.v1.serverPools.store'), $data)
+            ->assertUnauthorized();
+
+        // Test logged in users
+        $this->actingAs($this->user)->postJson(route('api.v1.serverPools.store'), $data)
+            ->assertForbidden();
+
+        // Authorize user
+        $role       = factory(Role::class)->create();
+        $permission = factory(Permission::class)->create(['name' => 'serverPools.create']);
+        $role->permissions()->attach($permission);
+        $this->user->roles()->attach($role);
+
+        // Test with authorized user, invalid server is tested and set to offline
+        $this->actingAs($this->user)->postJson(route('api.v1.serverPools.store'), $data)
+            ->assertSuccessful()
+            ->assertJsonFragment(
+                ['name'=>$serverPool->name,'description'=>$serverPool->description,'server_count'=>0,'model_name'=>'ServerPool']
+            );
+
+        // Test with some existing name
+        $this->actingAs($this->user)->postJson(route('api.v1.serverPools.store'), $data)
+            ->assertJsonValidationErrors(['name']);
+
+        // Test with invalid data
+        $data['name']    = '';
+        $data['servers'] = 'test';
+        $this->actingAs($this->user)->postJson(route('api.v1.serverPools.store'), $data)
+            ->assertJsonValidationErrors(['name','servers']);
+
+        // Create servers
+        $servers = factory(Server::class, 2)->create();
+
+        // Test with invalid servers value
+        $data['name']    = 'TestPool';
+        $data['servers'] = $servers[0]->id;
+        $this->actingAs($this->user)->postJson(route('api.v1.serverPools.store'), $data)
+            ->assertJsonValidationErrors(['servers']);
+
+        // Test with duplicated servers
+        $data['name']    = 'TestPool';
+        $data['servers'] = [$servers[0]->id,$servers[0]->id];
+        $this->actingAs($this->user)->postJson(route('api.v1.serverPools.store'), $data)
+            ->assertJsonValidationErrors(['servers.0','servers.1']);
+
+        // Test with invalid servers
+        $data['name']    = 'TestPool';
+        $data['servers'] = [0,$servers[0]->id];
+        $this->actingAs($this->user)->postJson(route('api.v1.serverPools.store'), $data)
+            ->assertJsonValidationErrors(['servers.0']);
+
+        // Test with servers
+        $data['name']    = 'TestPool';
+        $data['servers'] = [$servers[0]->id,$servers[1]->id];
+        $this->actingAs($this->user)->postJson(route('api.v1.serverPools.store'), $data)
+            ->assertSuccessful()
+            ->assertJsonFragment(
+                ['name'=>'TestPool','description'=>$serverPool->description,'server_count'=>2]
+            )
+            ->assertJsonPath('data.servers.0.id', $servers[0]->id)
+            ->assertJsonPath('data.servers.1.id', $servers[1]->id);
+    }
+
+    /**
+     * Test to update a server pool
+     */
+    public function testUpdate()
+    {
+        $serverPool  = factory(ServerPool::class)->create();
+        $serverPool2 = factory(ServerPool::class)->create();
+
+        $data = [
+            'name'        => $serverPool->name,
+            'description' => $serverPool->description,
+        ];
+
+        // Test guests
+        $this->putJson(route('api.v1.serverPools.update', ['serverPool'=>$serverPool->id]), $data)
+            ->assertUnauthorized();
+
+        // Test logged in users
+        $this->actingAs($this->user)->putJson(route('api.v1.serverPools.update', ['serverPool'=>$serverPool->id]), $data)
+            ->assertForbidden();
+
+        // Authorize user
+        $role       = factory(Role::class)->create();
+        $permission = factory(Permission::class)->create(['name' => 'serverPools.update']);
+        $role->permissions()->attach($permission);
+        $this->user->roles()->attach($role);
+
+        // Test with authorized user, without updated at
+        $this->actingAs($this->user)->putJson(route('api.v1.serverPools.update', ['serverPool'=>$serverPool->id]), $data)
+            ->assertStatus(CustomStatusCodes::STALE_MODEL);
+
+        $data['updated_at'] = $serverPool->updated_at;
+
+        // Test with authorized user, with updated at
+        $this->actingAs($this->user)->putJson(route('api.v1.serverPools.update', ['serverPool'=>$serverPool->id]), $data)
+            ->assertSuccessful()
+            ->assertJsonFragment(
+                ['name'=>$serverPool->name,'description'=>$serverPool->description,'server_count'=>0]
+            );
+
+        // Test with name of an other server pool
+        $serverPool->refresh();
+        $data['name']       = $serverPool2->name;
+        $data['updated_at'] = $serverPool->updated_at;
+        $this->actingAs($this->user)->putJson(route('api.v1.serverPools.update', ['serverPool'=>$serverPool->id]), $data)
+            ->assertJsonValidationErrors(['name']);
+
+        // Test with invalid data
+        $serverPool->refresh();
+        $data['name']          = '';
+        $data['servers']       = 'test';
+        $data['updated_at']    = $serverPool->updated_at;
+        $this->actingAs($this->user)->putJson(route('api.v1.serverPools.update', ['serverPool'=>$serverPool->id]), $data)
+            ->assertJsonValidationErrors(['name','servers']);
+
+        // Create servers
+        $servers = factory(Server::class, 2)->create();
+
+        // Add two servers
+        $serverPool->refresh();
+        $data['name']          = 'TestPool';
+        $data['servers']       = [$servers[0]->id,$servers[1]->id];
+        $data['updated_at']    = $serverPool->updated_at;
+        $this->actingAs($this->user)->putJson(route('api.v1.serverPools.update', ['serverPool'=>$serverPool->id]), $data)
+            ->assertJsonFragment(['name'=>'TestPool','description'=>$serverPool->description,'server_count'=>2])
+            ->assertJsonPath('data.servers.0.id', $servers[0]->id)
+            ->assertJsonPath('data.servers.1.id', $servers[1]->id);
+
+        // Remove one server
+        $serverPool->refresh();
+        $data['name']          = 'TestPool';
+        $data['servers']       = [$servers[0]->id];
+        $data['updated_at']    = $serverPool->updated_at;
+        $this->actingAs($this->user)->putJson(route('api.v1.serverPools.update', ['serverPool'=>$serverPool->id]), $data)
+            ->assertJsonFragment(['name'=>'TestPool','description'=>$serverPool->description,'server_count'=>1])
+            ->assertJsonPath('data.servers.0.id', $servers[0]->id);
+
+        // Remove all servers
+        $serverPool->refresh();
+        $data['name']          = 'TestPool';
+        $data['updated_at']    = $serverPool->updated_at;
+        unset($data['servers']);
+        $this->actingAs($this->user)->putJson(route('api.v1.serverPools.update', ['serverPool'=>$serverPool->id]), $data)
+            ->assertJsonFragment(['name'=>'TestPool','description'=>$serverPool->description,'server_count'=>0]);
+
+        // Test deleted
+        $serverPool->delete();
+        $this->actingAs($this->user)->putJson(route('api.v1.serverPools.update', ['serverPool'=>$serverPool->id]), $data)
+            ->assertNotFound();
+    }
+
+    /**
+     * Test to delete a server pool
+     */
+    public function testDelete()
+    {
+        $serverPool = factory(ServerPool::class)->create();
+        $roomTypes  = factory(RoomType::class, 2)->create(['server_pool_id'=>$serverPool]);
+
+        // Test guests
+        $this->deleteJson(route('api.v1.serverPools.destroy', ['serverPool'=>$serverPool->id]))
+            ->assertUnauthorized();
+
+        // Test logged in users
+        $this->actingAs($this->user)->deleteJson(route('api.v1.serverPools.destroy', ['serverPool'=>$serverPool->id]))
+            ->assertForbidden();
+
+        // Authorize user
+        $role       = factory(Role::class)->create();
+        $permission = factory(Permission::class)->create(['name' => 'serverPools.delete']);
+        $role->permissions()->attach($permission);
+        $this->user->roles()->attach($role);
+
+        // Test delete with room type attached
+        $this->actingAs($this->user)->deleteJson(route('api.v1.serverPools.destroy', ['serverPool'=>$serverPool->id]))
+            ->assertStatus(CustomStatusCodes::STALE_MODEL)
+            ->assertJsonCount(2, 'roomTypes')
+            ->assertJsonFragment(['id'=>$roomTypes[0]->id,'short'=>$roomTypes[0]->short,'description'=>$roomTypes[0]->description]);
+        $roomTypes[0]->delete();
+        $roomTypes[1]->delete();
+
+        // Test delete
+        $this->actingAs($this->user)->deleteJson(route('api.v1.serverPools.destroy', ['serverPool'=>$serverPool->id]))
+            ->assertSuccessful();
+
+        // Test delete again
+        $this->actingAs($this->user)->deleteJson(route('api.v1.serverPools.destroy', ['serverPool'=>$serverPool->id]))
+            ->assertNotFound();
+
+        $this->assertDatabaseMissing('servers', ['id'=>$serverPool->id]);
+    }
+}
