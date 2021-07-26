98df513533c3b252a1bde0d18d03058d99c7565a
Samuel Weirich <samuel.weirich@gmx.de>
2021-07-26 11:09:40 +0200

    Update validation rule

--- app/Http/Requests/UpdateSetting.php
+++ app/Http/Requests/UpdateSetting.php
@@ -49,8 +49,8 @@ class UpdateSetting extends FormRequest
             'attendance.enabled'                   => 'required|boolean',
             'attendance.retention_period'          => 'required|numeric|min:1|max:365',
             'bbb.logo'                             => 'nullable|string|max:255',
-            'bbb.logo_file'                        => 'nullable|image|max:500', // 500 KB, larger files are bad for loading times
-            'bbb.style'                            => ['nullable','file','max:500'],
+            'bbb.logo_file'                        => 'nullable|image|max:500',
+            'bbb.style'                            => 'nullable|file|max:500',
         ];
     }
 }
