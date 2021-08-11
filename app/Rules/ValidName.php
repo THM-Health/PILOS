461bcbae4eab8a52c8198df4fcffec3ada2940dc
Samuel Weirich <samuel.weirich@gmx.de>
2021-08-11 13:59:47 +0200

    Add name validation rule and adjust frontend to display errors

---
+++ app/Rules/ValidName.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class ValidName implements Rule
+{
+    const ALLOWED_CHARS = "A-Za-zàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆŠŽ∂ð ,.'-+/&";
+    private $failedChars;
+    private $attribute;
+
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        if (preg_match('#^['.self::ALLOWED_CHARS.']+$#', $value)) {
+            return true;
+        }
+        $this->attribute   = $attribute;
+        $this->failedChars = array_unique(str_split(preg_replace('#['.self::ALLOWED_CHARS.']+#', '', $value)));
+
+        return false;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        $invalidChars = implode('', $this->failedChars);
+        $validUTF8    = mb_check_encoding($invalidChars, 'UTF-8');
+        if ($validUTF8) {
+            return trans('validation.validname', ['chars'=> $invalidChars]);
+        } else {
+            return trans('validation.validname_error');
+        }
+    }
+}
