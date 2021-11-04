0fba09949627487c0ee0175c989b9f1d39ad4b57
Samuel Weirich <samuel.weirich@gmx.de>
2021-11-04 09:55:04 +0100

    Move ldap role mapping from event to controller

--- app/Http/Controllers/api/v1/auth/LoginController.php
+++ app/Http/Controllers/api/v1/auth/LoginController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers\api\v1\auth;
 
 use App\Http\Controllers\Controller;
+use App\Role;
+use Illuminate\Auth\Events\Authenticated;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
+use LdapRecord\Models\ModelNotFoundException;
+use LdapRecord\Models\OpenLDAP\User;
 
 class LoginController extends Controller
 {
@@ -103,7 +107,47 @@ class LoginController extends Controller
     protected function authenticated(Request $request, $user)
     {
         if (config('auth.log.successful')) {
-            Log::info('User ['.($user->authenticator == 'ldap' ? $user->username : $user->email).'] has been successfully authenticated.', ['ip'=>$request->ip(),'user-agent'=>$request->header('User-Agent'),'authenticator'=>$user->authenticator]);
+            Log::info('User [' . ($user->authenticator == 'ldap' ? $user->username : $user->email) . '] has been successfully authenticated.', ['ip' => $request->ip(), 'user-agent' => $request->header('User-Agent'), 'authenticator' => $user->authenticator]);
+        }
+
+        if ($user->authenticator === 'ldap') {
+            $this->mapLdapRoles($user);
+        }
+    }
+
+    /**
+     * Adds the roles from the ldap user to the application user, which are mapped
+     * in the config `ldap.roleMap`.
+     *
+     * @param  $user
+     * @throws ModelNotFoundException
+     */
+    protected function mapLdapRoles($user)
+    {
+        $ldapRoleAttribute = config('ldap.ldapRoleAttribute');
+        $ldapUser          = User::findByGuidOrFail($user->getLdapGuid());
+
+        if ($ldapUser->hasAttribute($ldapRoleAttribute)) {
+            $ldapRoles = $ldapUser->getAttribute($ldapRoleAttribute);
+
+            if (config('auth.log.ldap_roles')) {
+                \Log::debug('LDAP roles found for user ['.$user->username.'].', $ldapRoles);
+            }
+
+            $roleIds   = [];
+
+            foreach ($ldapRoles as $ldapRole) {
+                if (array_key_exists($ldapRole, config('ldap.roleMap'))) {
+                    $role = Role::where('name', config('ldap.roleMap')[$ldapRole])->first();
+
+                    if (!empty($role)) {
+                        $roleIds[$role->id] = ['automatic' => true];
+                    }
+                }
+            }
+
+            $user->roles()->syncWithoutDetaching($roleIds);
+            $user->roles()->detach($user->roles()->wherePivot('automatic', '=', true)->whereNotIn('role_id', array_keys($roleIds))->pluck('role_id')->toArray());
         }
     }
 }
