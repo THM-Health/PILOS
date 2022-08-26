0e8d276be34c7ed41729ad1b6b078155bcdc4c52
Samuel Weirich <4281791+SamuelWei@users.noreply.github.com>
2022-08-26 11:21:56 +0200

    Add frontend build caching

---
+++ docker/app/frontend-hash.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Setup vars
+MD5FILE=frontend.md5
+PREVIOUSMD5=""
+
+# If there was a previous hash get it if not create the file for later
+if [ -f "$MD5FILE" ]; then
+  PREVIOUSMD5="$(<"$MD5FILE")"
+else
+  "touch" "$MD5FILE"
+fi
+
+# Get hash of the build relevant MIX_ env variables
+MD5=$(printenv | grep MIX_ | md5sum | awk '{ print $1 }')
+
+# Get hash for every frontend file
+for DIR in "./resources"
+do
+  MD5+=$(find "$DIR" -type f -exec md5sum {} \; | md5sum | awk '{ print $1 }')
+done
+
+# Compare hashes
+if [ "$MD5" != "$PREVIOUSMD5" ]; then
+  echo "Frontend hash changed"
+  echo "Rebuild frontend"
+  # Run run prod
+  npm run prod
+  # Put new hash in the file for next time
+  echo "$MD5" > "$MD5FILE"
+  echo "Saved new hash"
+else
+  echo "Frontend hash unchanged, don't rebuild frontend"
+fi
