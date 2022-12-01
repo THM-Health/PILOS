9295794bc6884605e2d55da87f190cc7985a6db7
Samuel Weirich <4281791+SamuelWei@users.noreply.github.com>
2022-12-01 14:05:39 +0100

    Fix api test

--- tests/Frontend/api/base.spec.js
+++ tests/Frontend/api/base.spec.js
@@ -2,6 +2,8 @@ import Base from '../../../resources/js/api/base';
 import moxios from 'moxios';
 import VueRouter from 'vue-router';
 import { waitMoxios } from '../helper';
+import {createTestingPinia} from "@pinia/testing";
+import {useAuthStore} from "../../../resources/js/stores/auth";
 
 let consoleErrorStub;
 
@@ -79,8 +81,12 @@ describe('base', () => {
         commit: storeCommitSpy
       };
 
+      const pinia = createTestingPinia();
+      const auth = useAuthStore();
+      auth.currentUser = { id: 1 };
+
       const vm = {
-        $store: store,
+        $pinia: pinia,
         $router: router,
         flashMessage: flashMessage,
         $t: (key, values) => key + (values !== undefined ? ':' + JSON.stringify(values) : '')
@@ -97,8 +103,9 @@ describe('base', () => {
       expect(flashMessageInfoSpy).toBeCalledTimes(1);
       expect(flashMessageInfoSpy).toBeCalledWith('app.flash.unauthenticated');
 
-      expect(storeCommitSpy).toBeCalledTimes(1);
-      expect(storeCommitSpy).toBeCalledWith('session/setCurrentUser', { currentUser: null, emit: false });
+
+      expect(auth.setCurrentUser).toBeCalledTimes(1);
+      expect(auth.setCurrentUser).toBeCalledWith(null, false);
 
       jest.clearAllMocks();
 
