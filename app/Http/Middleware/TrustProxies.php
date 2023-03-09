d1a0f9d911829fc8591cfc94af4825a86944df61
Samuel Weirich <4281791+SamuelWei@users.noreply.github.com>
2023-03-09 16:44:44 +0100

    Fix cs

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -26,7 +26,6 @@ class TrustProxies extends Middleware
         Request::HEADER_X_FORWARDED_PROTO |
         Request::HEADER_X_FORWARDED_AWS_ELB;
 
-
     public function __construct()
     {
         $this->proxies = config('app.trusted_proxies');
