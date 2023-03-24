2e96699d650e51f6acaf87649f69997a63c2153e
Samuel Weirich <4281791+SamuelWei@users.noreply.github.com>
2023-03-24 16:07:12 +0100

    Add version to footer

--- resources/js/components/FooterComponent.vue
+++ resources/js/components/FooterComponent.vue
@@ -1,17 +1,23 @@
 <template>
   <footer class="footer">
-    <b-container class="border-top bg-white" v-if="getSetting('legal_notice_url') || getSetting('privacy_policy_url')" fluid>
-      <b-row align-h="center" align-v="center" ref="url_footer">
-        <a :href="getSetting('legal_notice_url')" v-if="getSetting('legal_notice_url')">
-          <small>{{$t('app.footer.legal_notice')}}</small>
-        </a>
+    <b-container class="border-top bg-white" fluid v-if="getSetting('legal_notice_url') || getSetting('privacy_policy_url') || getSetting('version')">
+      <b-container>
+      <div class="d-flex justify-content-between">
+        <div>
+          <a :href="getSetting('legal_notice_url')" v-if="getSetting('legal_notice_url')" class="d-inline">
+            <small>{{$t('app.footer.legal_notice')}}</small>
+          </a>
 
-        <div class="mx-2" v-if="getSetting('legal_notice_url') && getSetting('privacy_policy_url')"><small><raw-text>|</raw-text></small></div>
-
-        <a :href="getSetting('privacy_policy_url')" v-if="getSetting('privacy_policy_url')">
-          <small>{{$t('app.footer.privacy_policy')}}</small>
-        </a>
-      </b-row>
+          <div class="mx-2 d-inline" v-if="getSetting('legal_notice_url') && getSetting('privacy_policy_url')"><small><raw-text>|</raw-text></small></div>
+          <a :href="getSetting('privacy_policy_url')" v-if="getSetting('privacy_policy_url')" class="d-inline">
+            <small>{{$t('app.footer.privacy_policy')}}</small>
+          </a>
+        </div>
+        <small v-if="getSetting('version')">
+          {{$t('app.version')}} {{getSetting('version')}}
+        </small>
+      </div>
+    </b-container>
     </b-container>
   </footer>
 </template>
