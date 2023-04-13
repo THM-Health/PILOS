c366061b8c61a424778adeac6a6864d023649672
Samuel Weirich <4281791+SamuelWei@users.noreply.github.com>
2023-04-13 17:35:28 +0200

    Implement OIDC auth
    Use jumbojett/OpenID-Connect-PHP as a baseline, but major changes for jwt handling,  optimised for laravel and removal of all unneeded features

---
+++ app/Auth/OIDC/OpenIDConnectNetworkException.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace App\Auth\OIDC;
+
+class OpenIDConnectNetworkException extends \Exception {}
---
+++ app/Auth/OIDC/OpenIDConnectValidationException.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace App\Auth\OIDC;
+
+class OpenIDConnectValidationException extends \Exception {}
