a982bf86657a1dc83b3fa9da230a387fd202f827
Samuel Weirich <4281791+SamuelWei@users.noreply.github.com>
2023-04-27 11:48:58 +0200

    Fix missing translation

--- resources/js/views/Logout.vue
+++ resources/js/views/Logout.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="row mt-4 mb-5">
       <div class="col-12 col-md-8 col-lg-6 offset-md-2 offset-lg-3">
-        <b-card title="Logout" bg-variant="white">
+        <b-card :title="$t('auth.logout')" bg-variant="white">
           <b-alert variant="success" show>{{ $t('auth.logout_success') }}</b-alert>
           <template #footer>
             <b-button variant="success" block :to="{ name: 'home'}">
