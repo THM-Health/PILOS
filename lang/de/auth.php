0d85f6d4ff4a6a9fc95fae435fe954d32b25c2f9
Samuel Weirich <4281791+SamuelWei@users.noreply.github.com>
2023-05-15 13:07:35 +0200

    Convert locales to laravel php arrays, add convert script

---
+++ lang/de/auth.php
@@ -0,0 +1,52 @@
+<?php
+
+return [
+    'change_email'     => 'E-Mail Adresse ändern',
+    'change_password'  => 'Passwort ändern',
+    'current_password' => 'Aktuelles Passwort',
+    'email'            => [
+      'tab_title' => 'Email',
+      'title'     => 'Mit einer Email anmelden',
+    ],
+    'error' => [
+      'missing_attributes' => 'Es fehlen Attribute für die Authentifizierung.',
+    ],
+    'external_id' => 'Externe ID',
+    'failed'      => 'Diese Kombination aus Zugangsdaten wurde nicht in unserer Datenbank gefunden.',
+    'flash'       => [
+      'login'             => 'Erfolgreich angemeldet',
+      'logout_all_others' => 'Die Abmeldung von allen anderen Geräten verlief erfolgreich!',
+      'logout_error'      => 'Abmelden fehlgeschlagen',
+      'password_changed'  => 'Passwort erfolgreich geändert',
+    ],
+    'forgot_password'             => 'Passwort vergessen?',
+    'input_new_password'          => 'Neues Passwort setzen',
+    'input_new_password_new_user' => 'Passwort für neues Benutzerkonto setzen',
+    'ldap'                        => [
+      'tab_title'     => 'LDAP',
+      'title'         => 'Mit einem LDAP-Account anmelden',
+      'username'      => 'Benutzerkennung',
+      'username_help' => 'Geben Sie die Benutzerkennung (nicht die Email) Ihres LDAP-Account an',
+    ],
+    'login'                     => 'Anmelden',
+    'logout'                    => 'Abmelden',
+    'logout_success'            => 'Erfolgreich abgemeldet',
+    'new_password'              => 'Neues Passwort',
+    'new_password_confirmation' => 'Neues Passwort bestätigen',
+    'password'                  => 'Passwort',
+    'reset_password'            => 'Passwort zurücksetzen',
+    'send_email_confirm_mail'   => 'Es wurde eine Bestätigungsmail an {email} gesendet. Bestätigen Sie die Änderung, in dem Sie auf den Link in der E-Mail klicken.',
+    'send_password_reset_link'  => 'Passwort-Rücksetz-Link senden',
+    'sessions'                  => [
+      'active'        => 'Aktive Sitzungen',
+      'browser'       => 'Browser',
+      'current'       => 'Aktuelle Sitzung',
+      'ip'            => 'IP',
+      'last_active'   => 'Zuletzt aktiv',
+      'logout_all'    => 'Alle anderen Sitzungen abmelden',
+      'unknown_agent' => 'Unbekannt',
+    ],
+    'set_password'   => 'Passwort setzen',
+    'throttle'       => 'Zu viele Loginversuche. Versuchen Sie es bitte in :seconds Sekunden nochmal.',
+    'throttle_email' => 'Sie haben bereits vor kurzem eine Anfrage zur Änderung der E-Mail Adresse gestellt. Bitte versuchen Sie es zu einem späteren Zeitpunkt erneut.'
+];
---
+++ lang/de/home.php
@@ -0,0 +1,47 @@
+<?php
+
+return [
+    'description'          => 'Die Grundlage dafür ist das Open Source Projekt BigBlueButton.',
+    'description_extended' => 'Sie können für Ihre Veranstaltungen, Seminare und Übungen Räume erstellen und über einen praktischen Kurzlink Studenten, Mitarbeiter und Gäste einladen. Zusätzlich können Sie die Plattform auch für Prüfungen und Besprechnungen nutzen.',
+    'possibilities'        => [
+      'audio' => [
+        'description' => 'Kommuniziere über eine qualitativ hochwertige Audioverbindung',
+        'title'       => 'Audio',
+      ],
+      'chat' => [
+        'description' => 'Direktes Zuschauerfeedback',
+        'title'       => 'Emojis',
+      ],
+      'emojis' => [
+        'description' => 'Versende öffentliche und private Nachrichten',
+        'title'       => 'Chat',
+      ],
+      'groups' => [
+        'description' => 'Teile Nutzer für Gruppenarbeiten in Kleingruppen auf',
+        'title'       => 'Kleingruppen',
+      ],
+      'screen_sharing' => [
+        'description' => 'Teile den Inhalt deines Bildschirms oder Anwendung',
+        'title'       => 'Bildschirmfreigabe',
+      ],
+      'statistics' => [
+        'description' => 'Werte die Anwesenheit und Mediennutzung aus',
+        'title'       => 'Auswertungen',
+      ],
+      'surveys' => [
+        'description' => 'Interagiere zu jeder Zeit mit den Zuschauern',
+        'title'       => 'Umfragen',
+      ],
+      'title'   => 'Was PILOS alles kann',
+      'webcams' => [
+        'description' => 'Ermögliche persönliche visuelle Unterhaltungen',
+        'title'       => 'Webcams',
+      ],
+      'whiteboard' => [
+        'description' => 'Nutze eine gemeinsame interaktive virtuelle Tafel',
+        'title'       => 'Mehrnutzer Whiteboard',
+      ],
+    ],
+    'subtitle' => 'PILOS ist die Plattform für interaktive Live-Online-Seminare.',
+    'title'    => 'Willkommen zu PILOS.',
+];
