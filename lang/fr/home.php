cb16a2c24d82bac1045b2866ef9094f1c1899743
SamuelWei <SamuelWei@users.noreply.github.com>
2023-08-21 12:04:01 +0000

    [create-pull-request] automated change

--- lang/fr/home.php
+++ lang/fr/home.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'description'          => "L'application est basée sur le projet open source BigBlueButton.",
-    'description_extended' => "Vous pouvez créer des salles pour vos conférences, séminaires et tutoriels puis transmettre le lien d'accès à remettre à des étudiants, employés ou autre invités. Vous pouvez également utiliser la plate-forme pour les examens et les réunions.",
+    'description'          => 'L\'application est basée sur le projet open source BigBlueButton.',
+    'description_extended' => 'Vous pouvez créer des salles pour vos conférences, séminaires et tutoriels puis transmettre le lien d\'accès à remettre à des étudiants, employés ou autre invités. Vous pouvez également utiliser la plate-forme pour les examens et les réunions.',
     'possibilities'        => [
         'audio' => [
             'description' => 'Communiquez via une connexion audio de haute qualité',
@@ -17,15 +17,15 @@ return [
             'title'       => 'Chat',
         ],
         'groups' => [
-            'description' => "Répartissez les utilisateurs en petits groupes pour les travaux d'équipe",
+            'description' => 'Répartissez les utilisateurs en petits groupes pour les travaux d\'équipe',
             'title'       => 'Sous-groupes',
         ],
         'screen_sharing' => [
             'description' => 'Partagez votre écran ou une application spécifique',
-            'title'       => "Partage d'écran",
+            'title'       => 'Partage d\'écran',
         ],
         'statistics' => [
-            'description' => "Mesurez la fréquentation et l'utilisation de l'audio/vidéo",
+            'description' => 'Mesurez la fréquentation et l\'utilisation de l\'audio/vidéo',
             'title'       => 'Rapports',
         ],
         'surveys' => [
