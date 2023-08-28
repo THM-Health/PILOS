ba82270b61ebcb897f0b25466339a8175165752b
Daniel Machill <141118462+danielmachill@users.noreply.github.com>
2023-08-28 11:16:29 +0200

    Auto stash before merge of "302-default-room-role-and-waiting-room" and "origin/302-default-room-role-and-waiting-room"
    update language files

--- lang/en/rooms.php
+++ lang/en/rooms.php
@@ -235,7 +235,7 @@ return [
                 'only_for_guests_enabled' => 'Enabled only for guests',
                 'title'                   => 'Waiting room',
             ],
-            'waiting_room_alert'=> 'Default role is set to Moderator: Waiting room is only active for guests and old members!',
+            'waiting_room_alert'=> 'Default role is set to moderator: Waiting room is active only for guests and already existing members with participant role!',
         ],
         'permissions' => [
             'everyone_start' => 'Everyone can start the meeting',
