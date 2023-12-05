3ee6662ca2d001d368c05b33de5a4a07ae9dd900
Samuel Weirich <4281791+SamuelWei@users.noreply.github.com>
2023-12-05 14:15:23 +0100

    Replace color validation with offical rule

--- app/Http/Requests/RoomTypeRequest.php
+++ app/Http/Requests/RoomTypeRequest.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Requests;
 
-use App\Rules\Color;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
@@ -13,7 +12,7 @@ class RoomTypeRequest extends FormRequest
         $rules = [
             'description'   => ['required', 'string', 'max:255', Rule::unique('room_types', 'description')],
             'short'         => ['required', 'string', 'max:2', Rule::unique('room_types', 'short')],
-            'color'         => ['required', 'string', new Color()],
+            'color'         => ['required', 'string', 'hex_color'],
             'allow_listing' => ['required', 'boolean'],
             'server_pool'   => 'required|exists:App\Models\ServerPool,id',
             'restrict'      => ['required', 'boolean'],
