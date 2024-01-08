a2fddaeb1b8d58b95660f824e15f468a1e4e2d3d
Samuel Weirich <4281791+SamuelWei@users.noreply.github.com>
2024-01-08 11:57:18 +0100

    Add docs

--- app/Pulse/Users.php
+++ app/Pulse/Users.php
@@ -9,11 +9,26 @@ use Laravel\Pulse\Contracts\ResolvesUsers;
 
 class Users implements ResolvesUsers
 {
+    /**
+     * The resolved users.
+     *
+     * @var Collection<int, \Illuminate\Contracts\Auth\Authenticatable>
+     */
+    protected Collection $resolvedUsers;
+
+    /**
+     * Return a unique key identifying the user.
+     */
     public function key(Authenticatable $user): int|string|null
     {
         return $user->getAuthIdentifier();
     }
 
+    /**
+     * Eager load the users with the given keys.
+     *
+     * @param  Collection<int, int|string|null>  $keys
+     */
     public function load(Collection $keys): ResolvesUsers
     {
         $this->resolvedUsers = User::findMany($keys);
@@ -21,6 +36,11 @@ class Users implements ResolvesUsers
         return $this;
     }
 
+    /**
+     * Find the user with the given key.
+     *
+     * @return object{name: string, extra?: string, avatar?: string}
+     */
     public function find(int|string|null $key): object
     {
         $user = $this->resolvedUsers->first(fn ($user) => $this->key($user) == $key);
