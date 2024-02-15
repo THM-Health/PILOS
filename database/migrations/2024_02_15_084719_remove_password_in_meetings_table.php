6fa6ad4b1c084eabc92fa1064b2229b901a0409e
Samuel Weirich <4281791+SamuelWei@users.noreply.github.com>
2024-02-15 09:58:02 +0100

    Remove passwords

---
+++ database/migrations/2024_02_15_084719_remove_password_in_meetings_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('meetings', function (Blueprint $table) {
+            $table->dropColumn(['attendee_pw', 'moderator_pw']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('meetings', function (Blueprint $table) {
+            $table->string('attendee_pw',64)->nullable();
+            $table->string('moderator_pw',64)->nullable();
+        });
+    }
+};
