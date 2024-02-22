767abc8824b2f9308871532927b353ad744ac372
Samuel Weirich <4281791+SamuelWei@users.noreply.github.com>
2024-02-22 09:08:15 +0100

    Add loading indicator to room favorites component

--- resources/js/components/RoomFavoriteButton.vue
+++ resources/js/components/RoomFavoriteButton.vue
@@ -4,13 +4,15 @@
     v-tooltip="props.room.is_favorite ? $t('rooms.favorites.remove') : $t('rooms.favorites.add')"
     :aria-label="props.room.is_favorite ? $t('rooms.favorites.remove') : $t('rooms.favorites.add')"
     icon="fa-solid fa-star"
-    size="small"
     @click.stop="toggleFavorite"
+    :disabled="isLoading"
+    :loading="isLoading"
   />
 </template>
 
 <script setup>
 import { useApi } from '../composables/useApi.js';
+import { ref } from 'vue';
 
 const api = useApi();
 
@@ -18,12 +20,15 @@ const props = defineProps({
   room: Object
 });
 
+const isLoading = ref(false);
+
 const emit = defineEmits(['favoritesChanged']);
 
 /**
  * Add a room to the favorites or delete it from the favorites
  */
 function toggleFavorite () {
+  isLoading.value = true;
   let config;
   // check if the room must be added or deleted
   if (props.room.is_favorite) {
@@ -37,6 +42,7 @@ function toggleFavorite () {
       api.error(error);
     }).finally(() => {
       emit('favoritesChanged');
+      isLoading.value = false;
     });
 }
 </script>
