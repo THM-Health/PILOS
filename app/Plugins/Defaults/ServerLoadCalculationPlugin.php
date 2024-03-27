c99d252c132c40816e5797668e31afdd5a6ba49c
Samuel Weirich <4281791+SamuelWei@users.noreply.github.com>
2024-03-27 12:08:30 +0100

    Improve the calculation using the max amount of user defined for a meeting

--- app/Plugins/Defaults/ServerLoadCalculationPlugin.php
+++ app/Plugins/Defaults/ServerLoadCalculationPlugin.php
@@ -19,7 +19,18 @@ class ServerLoadCalculationPlugin implements ServerLoadCalculationPluginContract
             $createdAt = Carbon::createFromTimestampMsUTC($meeting->getCreationTime());
 
             if ($createdAt->diffInMinutes(now()) < config('bigbluebutton.load_new_meeting_min_user_interval')) {
-                $load += max(config('bigbluebutton.load_new_meeting_min_user_count'), $meeting->getParticipantCount());
+
+                // If meeting is in the starting phase, we use a higher number of users to calculate
+                // the load to compensate that the meeting will probably have more users in the future
+                $minUserCount = config('bigbluebutton.load_new_meeting_min_user_count');
+
+                // However if the meeting has a max user limit the meeting will never go over that limit,
+                // so we use the max user limit to calculate the load if it is lower than the min user count
+                if ($meeting->getMaxUsers() > 0) {
+                    $minUserCount = min($minUserCount, $meeting->getMaxUsers());
+                }
+
+                $load += max($minUserCount, $meeting->getParticipantCount());
             } else {
                 $load += $meeting->getParticipantCount();
             }
