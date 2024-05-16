dbd06d0c737c6b990b5c01a334d2d9f241db4925
Samuel Weirich <4281791+SamuelWei@users.noreply.github.com>
2024-05-16 20:39:07 +0200

    Refactor cleanup rooms command

--- app/Console/Commands/CleanupRoomsCommand.php
+++ app/Console/Commands/CleanupRoomsCommand.php
@@ -22,45 +22,43 @@ class CleanupRoomsCommand extends Command
         $neverUsedPeriod = app(RoomSettings::class)->auto_delete_never_used_period;
         $deadlinePeriod = app(RoomSettings::class)->auto_delete_deadline_period;
 
-        // Check if both periods are unlimited, in that case we don't need to do anything
-        // Do not remove, as this would result in the query to not filter at all!
-        if ($inactivePeriod == TimePeriod::UNLIMITED && $neverUsedPeriod == TimePeriod::UNLIMITED) {
-            return;
-        }
+        // Check if at least on period is enabled, otherwise skip marking rooms for deletion and sending emails
+        if ($inactivePeriod != TimePeriod::UNLIMITED || $neverUsedPeriod != TimePeriod::UNLIMITED) {
 
-        $lastStartDate = now()->subDays($inactivePeriod->value);
-        $createdDate = now()->subDays($neverUsedPeriod->value);
-        $timeToDeleteDate = now()->addDays($deadlinePeriod->value);
+            $lastStartDate = now()->subDays($inactivePeriod->value);
+            $createdDate = now()->subDays($neverUsedPeriod->value);
+            $timeToDeleteDate = now()->addDays($deadlinePeriod->value);
 
-        // Find ids of rooms that have never been used of the last meeting was a long time ago
-        $inactiveRoomIDs = DB::table('rooms')
-            ->leftJoin('meetings', 'rooms.meeting_id', '=', 'meetings.id')
-            ->join('users', 'rooms.user_id', '=', 'users.id')
-            ->select('rooms.id')
-            ->whereNull('delete_inactive')
-            ->where(function ($query) use ($neverUsedPeriod, $inactivePeriod, $createdDate, $lastStartDate) {
-                $query->where(function ($query) use ($inactivePeriod, $lastStartDate) {
-                    if ($inactivePeriod != TimePeriod::UNLIMITED) {
-                        $query->where('meetings.start', '<', $lastStartDate);
-                    }
-                })
-                    ->orWhere(function ($query) use ($neverUsedPeriod, $createdDate) {
-                        if ($neverUsedPeriod != TimePeriod::UNLIMITED) {
-                            $query->where('rooms.created_at', '<', $createdDate)
-                                ->whereNull('meetings.start');
+            // Find ids of rooms that have never been used of the last meeting was a long time ago
+            $inactiveRoomIDs = DB::table('rooms')
+                ->leftJoin('meetings', 'rooms.meeting_id', '=', 'meetings.id')
+                ->join('users', 'rooms.user_id', '=', 'users.id')
+                ->select('rooms.id')
+                ->whereNull('delete_inactive')
+                ->where(function ($query) use ($neverUsedPeriod, $inactivePeriod, $createdDate, $lastStartDate) {
+                    $query->where(function ($query) use ($inactivePeriod, $lastStartDate) {
+                        if ($inactivePeriod != TimePeriod::UNLIMITED) {
+                            $query->where('meetings.start', '<', $lastStartDate);
                         }
-                    });
-            })
-            ->pluck('rooms.id');
+                    })
+                        ->orWhere(function ($query) use ($neverUsedPeriod, $createdDate) {
+                            if ($neverUsedPeriod != TimePeriod::UNLIMITED) {
+                                $query->where('rooms.created_at', '<', $createdDate)
+                                    ->whereNull('meetings.start');
+                            }
+                        });
+                })
+                ->pluck('rooms.id');
 
-        // Load rooms, set delete date and send an email notification to the room owner
-        Log::info('Notifying '.count($inactiveRoomIDs).' room owners about their unused rooms');
-        foreach ($inactiveRoomIDs as $inactiveRoomID) {
-            $room = Room::find($inactiveRoomID);
-            $room->delete_inactive = $timeToDeleteDate;
-            $room->save();
+            // Load rooms, set delete date and send an email notification to the room owner
+            Log::info('Notifying '.count($inactiveRoomIDs).' room owners about their unused rooms');
+            foreach ($inactiveRoomIDs as $inactiveRoomID) {
+                $room = Room::find($inactiveRoomID);
+                $room->delete_inactive = $timeToDeleteDate;
+                $room->save();
 
-            $room->owner->notify(new RoomExpires($room));
+                $room->owner->notify(new RoomExpires($room));
+            }
         }
 
         // Delete all rooms after the grace period
