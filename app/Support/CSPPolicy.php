7b05d902861f50235ddc87d4efb33de7a3f54b1e
Samuel Weirich <4281791+SamuelWei@users.noreply.github.com>
2024-05-21 14:02:47 +0200

    Add vite dev server to CSP Policy

--- app/Support/CSPPolicy.php
+++ app/Support/CSPPolicy.php
@@ -20,5 +20,19 @@ class CSPPolicy extends Policy
             ->addDirective(Directive::OBJECT, Keyword::NONE)
             ->addDirective(Directive::STYLE, [Keyword::SELF, Keyword::UNSAFE_INLINE])
             ->addNonceForDirective(Directive::SCRIPT);
+
+        // Add Vite dev server to CSP in local environment if vite dev server is running
+        if (config('app.env') == 'local' && file_exists(public_path('hot'))) {
+            $viteURL = file_get_contents(public_path('hot'));
+            $viteURLParts = parse_url($viteURL);
+
+            $this->addDirective(Directive::BASE, $viteURL);
+            $this->addDirective(Directive::CONNECT, $viteURL);
+            $this->addDirective(Directive::CONNECT, 'wss://'.$viteURLParts['host'].':'.$viteURLParts['port']);
+            $this->addDirective(Directive::CONNECT, 'ws://'.$viteURLParts['host'].':'.$viteURLParts['port']);
+            $this->addDirective(Directive::DEFAULT, $viteURL);
+            $this->addDirective(Directive::MEDIA, $viteURL);
+            $this->addDirective(Directive::STYLE, $viteURL);
+        }
     }
 }
