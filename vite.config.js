738d692d88d2ed328979a31991546f9776169e68
Samuel Weirich <4281791+SamuelWei@users.noreply.github.com>
2024-07-15 11:08:27 +0200

    Fix docker file

--- vite.config.js
+++ vite.config.js
@@ -29,7 +29,7 @@ export default ({ mode }) => {
         input: [
           'resources/js/app.js',
           'resources/sass/app.scss'
-        ],
+        ]
       }),
       vue(),
       Components({
