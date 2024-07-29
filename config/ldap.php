9ff276cf98e8af1df873ca87543bfd91f746877e
Samuel Weirich <4281791+SamuelWei@users.noreply.github.com>
2024-07-29 12:50:57 +0200

    Set default ldap version to v3

--- config/ldap.php
+++ config/ldap.php
@@ -13,6 +13,7 @@ return [
         'port' => env('LDAP_PORT', 389),
         'base_dn' => env('LDAP_BASE_DN', 'dc=local,dc=com'),
         'timeout' => env('LDAP_TIMEOUT', 5),
+        'version' => env('LDAP_VERSION', 3),
         'use_ssl' => env('LDAP_SSL', false),
         'use_tls' => env('LDAP_TLS', false),
     ],
