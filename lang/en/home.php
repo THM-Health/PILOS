b12f6551f16827a295b110c3e898c142adc6f023
SamuelWei <SamuelWei@users.noreply.github.com>
2024-08-28 13:28:12 +0000

    Update locales

--- lang/en/home.php
+++ lang/en/home.php
@@ -1,53 +1,48 @@
 <?php
 
 return [
-
-    'title' => [
-        'first_line' => [
-            'before_flip_words' => 'Online ',
-            'flip_words' => 'Meetings|Lectures|Seminars|Exams',
-            'after_flip_words' => '',
-        ],
-        'second_line' => [
-            'before_underline' => 'made simple and ',
-            'underline' => 'secure',
-            'after_underline' => '.',
-        ],
-        'get_started' => 'Get started',
-    ],
+    'description_details' => 'Designed with education at its core, PILOS offers a seamless, interactive, and secure solution for online learning, teaching, and all kinds of virtual meetings. Adaptable to your needs, PILOS delivers high-quality video, robust collaboration tools, and an intuitive user experience.',
+    'description_intro' => 'All in one video conferencing platform powered by BigBlueButton™.',
     'features' => [
-
-        'vision' => 'Connecting People',
-        'title' => 'All you need for a great video conference experience',
-        'description' => 'Explore what makes PILOS the perfect solution for you next video conference and how it can help you reach your audience in an engaging and interactive way.',
-
-        'communication' => [
-            'title' => 'Video and Audio',
-            'description' => 'High quality video and audio to foster a more personal and engaging experience.',
-        ],
-        'engagement' => [
-            'title' => 'Engagement',
-            'description' => 'Engage your audience with interactive tools like chat, emojis, surveys and much more.',
-        ],
-        'security' => [
-            'title' => 'Security',
-            'description' => 'All communication data is securely encrypted during transmission using the WebRTC protocol.',
-        ],
         'collaboration' => [
-            'title' => 'Collaboration',
             'description' => 'Share your screen, use the interactive multi-user whiteboard, and work together in breakout rooms.',
+            'title' => 'Collaboration',
+        ],
+        'communication' => [
+            'description' => 'High quality video and audio to foster a more personal and engaging experience.',
+            'title' => 'Video and Audio',
+        ],
+        'description' => 'Explore what makes PILOS the perfect solution for you next video conference and how it can help you reach your audience in an engaging and interactive way.',
+        'engagement' => [
+            'description' => 'Engage your audience with interactive tools like chat, emojis, surveys and much more.',
+            'title' => 'Engagement',
         ],
         'recording' => [
-            'title' => 'Recording',
             'description' => 'Record your videoconferences and make them available using fine grained permission control.',
+            'title' => 'Recording',
+        ],
+        'security' => [
+            'description' => 'All communication data is securely encrypted during transmission using the WebRTC protocol.',
+            'title' => 'Security',
         ],
         'statistics' => [
-            'title' => 'Statistics',
             'description' => 'Get detailed reports on the attendance and usage of your videoconferences.',
+            'title' => 'Statistics',
         ],
-
+        'title' => 'All you need for a great video conference experience',
+        'vision' => 'Connecting People',
     ],
-
-    'description_intro' => 'All in one video conferencing platform powered by BigBlueButton™.',
-    'description_details' => 'Designed with education at its core, PILOS offers a seamless, interactive, and secure solution for online learning, teaching, and all kinds of virtual meetings. Adaptable to your needs, PILOS delivers high-quality video, robust collaboration tools, and an intuitive user experience.',
+    'title' => [
+        'first_line' => [
+            'after_flip_words' => ' ',
+            'before_flip_words' => 'Online ',
+            'flip_words' => 'Meetings|Lectures|Seminars|Exams',
+        ],
+        'get_started' => 'Get started',
+        'second_line' => [
+            'after_underline' => '.',
+            'before_underline' => 'made simple and ',
+            'underline' => 'secure',
+        ],
+    ],
 ];
