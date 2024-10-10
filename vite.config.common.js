fd323e693a160e777d7a763533fbba79d769ccb8
Samuel Weirich <4281791+SamuelWei@users.noreply.github.com>
2024-10-10 14:37:32 +0200

    Restructure vite for coverage

---
+++ vite.config.common.js
@@ -0,0 +1,67 @@
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+import * as fs from 'fs';
+import Components from 'unplugin-vue-components/vite';
+import { PrimeVueResolver } from '@primevue/auto-import-resolver';
+import { loadEnv } from 'vite';
+
+export default (mode) => {
+  const ENV_PREFIX = ['VITE_'];
+
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), ENV_PREFIX) };
+
+  const VITE_HMR_HOST = process.env.VITE_HOST || 'localhost';
+  const VITE_PORT = parseInt(process.env.VITE_PORT || 1073);
+
+  return {
+    plugins: [
+      laravel({
+        input: [
+          'resources/js/app.js',
+          'resources/sass/app.scss'
+        ]
+      }),
+      vue(),
+      Components({
+        dirs: ['resources/js', 'resources/custom/js'],
+        allowOverrides: true,
+        extensions: ['vue'],
+        deep: true,
+        dts: true,
+        resolvers: [
+          PrimeVueResolver()
+        ]
+      })
+    ],
+    server: {
+      https: getSslConfig(process.env),
+      host: '0.0.0.0',
+      port: VITE_PORT,
+      strictPort: true,
+      hmr: {
+        host: VITE_HMR_HOST
+      }
+    },
+    optimizeDeps: {
+      include: ['axe-core']
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler'
+        }
+      }
+    }
+  };
+};
+
+function getSslConfig (env) {
+  if (env.VITE_SSL !== 'true') {
+    return false;
+  }
+
+  return {
+    key: fs.readFileSync('ssl/privkey.pem'),
+    cert: fs.readFileSync('ssl/fullchain.pem')
+  };
+}
--- vite.config.js
+++ vite.config.js
@@ -1,71 +1,7 @@
-import { defineConfig, loadEnv } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
-import * as fs from 'fs';
-import Components from 'unplugin-vue-components/vite';
-import { PrimeVueResolver } from '@primevue/auto-import-resolver';
-import istanbul from 'vite-plugin-istanbul';
-
+import { defineConfig } from 'vite';
+import commonConfig from './vite.config.common';
 export default ({ mode }) => {
-  const ENV_PREFIX = ['VITE_'];
-
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), ENV_PREFIX) };
-
-  const VITE_HMR_HOST = process.env.VITE_HOST || 'localhost';
-  const VITE_PORT = parseInt(process.env.VITE_PORT || 1073);
-
-  function getSslConfig () {
-    if (process.env.VITE_SSL !== 'true') {
-      return false;
-    }
-
-    return {
-      key: fs.readFileSync('ssl/privkey.pem'),
-      cert: fs.readFileSync('ssl/fullchain.pem')
-    };
-  }
-  return defineConfig({
-    plugins: [
-      laravel({
-        input: [
-          'resources/js/app.js',
-          'resources/sass/app.scss'
-        ]
-      }),
-      vue(),
-      istanbul({
-        forceBuildInstrument: true,
-        requireEnv: true
-      }),
-      Components({
-        dirs: ['resources/js', 'resources/custom/js'],
-        allowOverrides: true,
-        extensions: ['vue'],
-        deep: true,
-        dts: true,
-        resolvers: [
-          PrimeVueResolver()
-        ]
-      })
-    ],
-    server: {
-      https: getSslConfig(),
-      host: true,
-      port: VITE_PORT,
-      strictPort: true,
-      hmr: {
-        host: VITE_HMR_HOST
-      }
-    },
-    optimizeDeps: {
-      include: ['axe-core']
-    },
-    css: {
-      preprocessorOptions: {
-        scss: {
-          api: 'modern-compiler'
-        }
-      }
-    }
-  });
+  return defineConfig(
+    commonConfig(mode)
+  );
 };
