140c4751273aeb6f4074853a5cf91457a84590c5
Samuel Weirich <4281791+SamuelWei@users.noreply.github.com>
2024-10-15 15:18:50 +0200

    Add Admin option to disable welcome page (#1441)
    * Add Admin option to disable welcome page
    
    * Update CHANGELOG
    
    * Add frontend tests

--- app/Http/Resources/Config.php
+++ app/Http/Resources/Config.php
@@ -39,6 +39,7 @@ class Config extends JsonResource
                 'help_url' => $generalSettings->help_url,
                 'legal_notice_url' => $generalSettings->legal_notice_url,
                 'privacy_policy_url' => $generalSettings->privacy_policy_url,
+                'no_welcome_page' => $generalSettings->no_welcome_page,
                 'default_locale' => config('app.locale'),
                 'enabled_locales' => array_map(function ($locale) {
                     return $locale['name'];
--- tests/Backend/Feature/api/v1/ApplicationTest.php
+++ tests/Backend/Feature/api/v1/ApplicationTest.php
@@ -37,6 +37,7 @@ class ApplicationTest extends TestCase
         $this->generalSettings->legal_notice_url = 'http://localhost';
         $this->generalSettings->privacy_policy_url = 'http://localhost';
         $this->generalSettings->toast_lifetime = 10;
+        $this->generalSettings->no_welcome_page = false;
         $this->generalSettings->save();
 
         $this->themeSettings->logo = 'testlogo.svg';
@@ -95,6 +96,7 @@ class ApplicationTest extends TestCase
                         'help_url' => 'http://localhost',
                         'legal_notice_url' => 'http://localhost',
                         'privacy_policy_url' => 'http://localhost',
+                        'no_welcome_page' => false,
                     ],
                     'theme' => [
                         'logo' => 'testlogo.svg',
@@ -147,6 +149,7 @@ class ApplicationTest extends TestCase
         $this->generalSettings->help_url = null;
         $this->generalSettings->legal_notice_url = null;
         $this->generalSettings->privacy_policy_url = null;
+        $this->generalSettings->no_welcome_page = true;
         $this->generalSettings->save();
 
         $this->bannerSettings->enabled = false;
@@ -177,6 +180,7 @@ class ApplicationTest extends TestCase
                         'help_url' => null,
                         'legal_notice_url' => null,
                         'privacy_policy_url' => null,
+                        'no_welcome_page' => true,
                     ],
                     'theme' => [
                         'logo' => 'testlogo.svg',
