40818e67e33abcbd978be9b71bebed28def381ef
Samuel Weirich <4281791+SamuelWei@users.noreply.github.com>
2024-11-22 17:07:33 +0100

    Add tests

--- tests/Backend/Unit/Console/ImportRecordingsCommandTest.php
+++ tests/Backend/Unit/Console/ImportRecordingsCommandTest.php
@@ -17,8 +17,45 @@ class ImportRecordingsCommandTest extends TestCase
 
         config(['filesystems.disks.recordings-spool.root' => 'tests/Backend/Fixtures/Recordings']);
 
-        $this->artisan('import:recordings');
+        $this->artisan('import:recordings')->assertSuccessful();
 
         Queue::assertCount(3);
+    }
+
+    public function testImportRecordingWithHook()
+    {
+        Queue::fake();
+
+        config(['filesystems.disks.recordings-spool.root' => 'tests/Backend/Fixtures/Recordings']);
+
+        // Import hook command to write "OK" to a temp file
+        $tempFile = tempnam(sys_get_temp_dir(), 'recording-import-hook-test');
+        config(['recording.import_before_hook' => 'echo "OK" > '.$tempFile]);
+
+        $this->artisan('import:recordings')->assertSuccessful();
+
+        // Check if hook was executed
+        $this->assertStringContainsString('OK', file_get_contents($tempFile));
+        // Clean up
+        unlink($tempFile);
+
+        // Check if the recordings were queued
+        Queue::assertCount(3);
+    }
+
+    public function testImportRecordingWithFailingHook()
+    {
+        Queue::fake();
+
+        config(['filesystems.disks.recordings-spool.root' => 'tests/Backend/Fixtures/Recordings']);
+
+        // Import hook command to write "OK" to a file that does not exist
+        $file = '/invalidPath/invalidFile';
+        config(['recording.import_before_hook' => 'echo "OK" > '.$file]);
+
+        $this->artisan('import:recordings')->assertFailed();
+
+        // Check if the recordings were not queued
+        Queue::assertCount(0);
     }
 }
