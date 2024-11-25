dbe8e819c5c9f915f99b16fce04cc8905563f42b
Samuel Weirich <4281791+SamuelWei@users.noreply.github.com>
2024-11-25 17:16:21 +0100

    Add command to test email config

---
+++ app/Console/Commands/TestEmailConfig.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+use function Laravel\Prompts\text;
+
+class TestEmailConfig extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'mail:test {email?}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send a test email to validate config';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        // Ask for the email address to send the test email
+        $email = $this->argument('email') ?: text('Recipient email address');
+
+        // Send the test email
+        try {
+            \Mail::raw('This is a test email', function ($message) use ($email) {
+                $message->to($email);
+                $message->subject('Test email');
+            });
+            $this->info('Test email sent successfully');
+        } catch (\Exception $e) {
+            $this->error('Failed to send test email: '.$e->getMessage());
+        }
+    }
+}
