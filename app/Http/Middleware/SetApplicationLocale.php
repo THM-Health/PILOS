2440b30934c38f56de2234d4d7a872aa21c1b89d
Samuel Weirich <4281791+SamuelWei@users.noreply.github.com>
2025-03-05 09:36:55 +0100

    If fallback locale is not enabled, use first enabled locale

--- app/Http/Middleware/SetApplicationLocale.php
+++ app/Http/Middleware/SetApplicationLocale.php
@@ -35,8 +35,17 @@ class SetApplicationLocale
             $locale = session()->get('locale');
         }
 
+        // If locale is not present in enabled locales, fallback to another locale
         if (! in_array($locale, array_keys(config('app.enabled_locales')))) {
-            $locale = config('app.fallback_locale');
+            $fallbackLocale = config('app.fallback_locale');
+
+            // If fallback locale is present in enabled locales, use it
+            if (in_array($fallbackLocale, array_keys(config('app.enabled_locales')))) {
+                $locale = $fallbackLocale;
+            } else {
+                // Otherwise, use the first enabled locale
+                $locale = array_key_first(config('app.enabled_locales'));
+            }
         }
 
         app()->setLocale($locale);
