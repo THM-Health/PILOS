73f777494cffe730497e39d24725a9cfea643fa1
Samuel Weirich <4281791+SamuelWei@users.noreply.github.com>
2025-10-09 15:02:34 +0200

    Fix tests

--- app/Services/AuthenticationService.php
+++ app/Services/AuthenticationService.php
@@ -43,8 +43,9 @@ class AuthenticationService
         // If session id provided, keep session alive, otherwise logout all sessions of the user
         if ($session) {
             $this->logoutOtherSessions($session);
+
             // Recreate new session for the requesting session
-            request()->session()->regenerate(true);
+            session()->regenerate(true);
         } else {
             $this->logoutAllSessions();
         }
