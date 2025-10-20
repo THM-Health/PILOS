THM-Health/PILOS

commit 621abf203cbd5df329598af1c84b458179e2f3ca

Migrate to Tailwind v4 (#2477)
* Migrate to Tailwind v4

* Fix css

* Cleanup, fix border color

* Fix table layout

* Fix cs

* Update changelog

* Add biome ignore for CodeRabbit

* Fix RoomCardSkeleton

* Fix mouse feedback for ColorSelect

* Move headers in AdminRolesView help modal to the left

* Fix padding inside AdminRolesView help modal

* Fix tests

* Change translations

* Fix heading sizing in AdminRolesView and AdminServersView

* Fix add button position on small devices (Admin index pages)

* Fix prettier tailwind config

Co-authored-by: Sabrina Wüst <119059706+Sabr1n4W@users.noreply.github.com>

resources/js/components/RoomTabSection.vue
@@ -3,7 +3,7 @@
     <template #header>
       <!-- Tab header -->
       <div
-        class="mb-5 flex flex-row justify-between border-b px-6 py-4 border-surface"
+        class="mb-5 flex flex-row justify-between border-b border-surface px-6 py-4"
       >
         <!-- Current tab -->
         <div class="flex flex-row items-center gap-2 px-2 text-xl">
@@ -101,7 +101,7 @@
     :draggable="false"
     :style="{ width: '25rem' }"
   >
-    <div class="mb-5 mt-4 flex justify-center">
+    <div class="mt-4 mb-5 flex justify-center">
       <Badge
         severity="contrast"
         class="flex h-16 w-16 items-center justify-center rounded-full"

resources/js/views/RoomsView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-cloak class="container mb-8 mt-8">
+  <div v-cloak class="container mt-8 mb-8">
     <!-- room token is invalid -->
     <div v-if="tokenInvalid" class="mt-20 flex justify-center">
       <!-- Show message that room can only be used by logged in users -->

commit f059d553b885a48d3ca49b02e420de54fc00964b

Fix: UI badges not round
Caused by https://github.com/THM-Health/PILOS/pull/2477

resources/js/components/RoomTabSection.vue
@@ -104,7 +104,7 @@
     <div class="mt-4 mb-5 flex justify-center">
       <Badge
         severity="contrast"
-        class="flex h-16 w-16 items-center justify-center rounded-full"
+        class="flex !h-16 !w-16 items-center justify-center rounded-full"
       >
         <i
           class="fa-solid fa-ban text-4xl text-white dark:text-surface-950"

resources/js/views/NotFound.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="mt-20 flex justify-center">
-    <!-- Show message that room can only be used by logged in users -->
+    <!-- Show 404 message -->
     <Card
       style="width: 500px; max-width: 90vw"
       :pt="{ header: { class: 'flex justify-center' } }"
@@ -8,7 +8,7 @@
       <template #header>
         <Badge
           severity="danger"
-          class="-mt-8 flex h-16 w-16 items-center justify-center rounded-full"
+          class="-mt-8 flex !h-16 !w-16 items-center justify-center rounded-full"
         >
           <i class="fa-solid fa-unlink text-2xl text-white"></i>
         </Badge>
@@ -20,3 +20,4 @@
     </Card>
   </div>
 </template>
+<script setup lang="ts"></script>

resources/js/views/RoomsView.vue
@@ -10,7 +10,7 @@
         <template #header>
           <Badge
             severity="danger"
-            class="-mt-8 flex h-16 w-16 items-center justify-center rounded-full"
+            class="-mt-8 flex !h-16 !w-16 items-center justify-center rounded-full"
           >
             <i class="fa-solid fa-unlink text-2xl text-white"></i>
           </Badge>
@@ -32,7 +32,7 @@
         <template #header>
           <Badge
             severity="danger"
-            class="-mt-8 flex h-16 w-16 items-center justify-center rounded-full"
+            class="-mt-8 flex !h-16 !w-16 items-center justify-center rounded-full"
           >
             <i class="fa-solid fa-lock text-2xl text-white"></i>
           </Badge>
@@ -86,7 +86,7 @@
             <template #header>
               <Badge
                 severity="danger"
-                class="-mt-8 flex h-16 w-16 items-center justify-center rounded-full"
+                class="-mt-8 flex !h-16 !w-16 items-center justify-center rounded-full"
               >
                 <i class="fa-solid fa-lock text-2xl text-white"></i>
               </Badge>