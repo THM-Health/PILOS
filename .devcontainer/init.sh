0a5785265af17278a8407c4721c754164e078a63
Samuel Weirich <4281791+samuelwei@users.noreply.github.com>
2025-12-15 20:34:11 +0000

    Fix missing user and group

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+export WWWUSER=$(id -u)
+export WWWGROUP=$(id -g)
+
 # Copy .env.example file
 cp .env.example .env
 
 sed -i 's|APP_ENV=production|APP_ENV=local|g' .env
 sed -i 's|APP_DEBUG=false|APP_DEBUG=true|g' .env
 sed -i 's|CONTAINER_DEV_IMAGE=pilos/pilos:local-dev|CONTAINER_DEV_IMAGE=pilos/pilos:dev-develop|g' .env
+
+sed -i "s|#WWWGROUP=1000|WWWGROUP=${WWWUSER}|g" .env
+sed -i "s|#WWWUSER=1000|WWWUSER=${WWWGROUP}|g" .env
