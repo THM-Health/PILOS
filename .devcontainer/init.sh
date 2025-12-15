7755a6bd1888375881ff22d342fdde85c1508b55
Samuel Weirich <4281791+samuelwei@users.noreply.github.com>
2025-12-15 20:15:57 +0000

    Move custom environment steps to later steps

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-IMAGE=pilos/pilos:dev-develop
-
 # Copy .env.example file
 cp .env.example .env
 
-export APP_KEY=base64:$(openssl rand -base64 32)
-
-sed -i "s|APP_URL=http://localhost|APP_URL=https://${CODESPACE_NAME}-80.app.github.dev|g" .env
-sed -i "s|APP_KEY=|APP_KEY=${APP_KEY}|g" .env
 sed -i 's|APP_ENV=production|APP_ENV=local|g' .env
 sed -i 's|APP_DEBUG=false|APP_DEBUG=true|g' .env
 sed -i 's|CONTAINER_DEV_IMAGE=pilos/pilos:local-dev|CONTAINER_DEV_IMAGE=pilos/pilos:dev-develop|g' .env
--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -2,3 +2,7 @@
 
 composer install
 npm install
+
+sed -i "s|APP_URL=http://localhost|APP_URL=https://${CODESPACE_NAME}-80.app.github.dev|g" .env
+
+php artisan key:generate
