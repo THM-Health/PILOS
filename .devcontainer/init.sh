def688f09a5b132e56f08a7597745f2a3cebfaae
Samuel Weirich <4281791+samuelwei@users.noreply.github.com>
2025-12-15 20:38:37 +0000

    Set fixed UID/GID

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-export WWWUSER=$(id -u)
-export WWWGROUP=$(id -g)
-
 # Copy .env.example file
 cp .env.example .env
 
@@ -10,5 +7,5 @@ sed -i 's|APP_ENV=production|APP_ENV=local|g' .env
 sed -i 's|APP_DEBUG=false|APP_DEBUG=true|g' .env
 sed -i 's|CONTAINER_DEV_IMAGE=pilos/pilos:local-dev|CONTAINER_DEV_IMAGE=pilos/pilos:dev-develop|g' .env
 
-sed -i "s|#WWWGROUP=1000|WWWGROUP=${WWWUSER}|g" .env
-sed -i "s|#WWWUSER=1000|WWWUSER=${WWWGROUP}|g" .env
+sed -i "s|#WWWGROUP=1000|WWWGROUP=1000|g" .env
+sed -i "s|#WWWUSER=1000|WWWUSER=1000|g" .env
