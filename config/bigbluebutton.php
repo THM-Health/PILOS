b645ff782b1d77aaa8de2f0cf79e473defc463b2
Samuel Weirich <4281791+SamuelWei@users.noreply.github.com>
2026-01-26 13:34:29 +0100

    Add tests

--- config/bigbluebutton.php
+++ config/bigbluebutton.php
@@ -24,7 +24,7 @@ return [
 
     'allowed_name_characters' => env('BBB_ALLOWED_NAME_CHARACTERS', "\w ,.'\-+\/&()"),
 
-    'server_hashing_algorithm' => match (env('BBB_SERVER_HASHING_ALGORITHM')) {
+    'server_hashing_algorithm' => match (strtoupper(env('BBB_SERVER_HASHING_ALGORITHM', 'SHA1'))) {
         'SHA256' => HashingAlgorithm::SHA_256,
         'SHA384' => HashingAlgorithm::SHA_384,
         'SHA512' => HashingAlgorithm::SHA_512,
