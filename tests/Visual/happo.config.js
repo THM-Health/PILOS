edf5cfab49f5f7124ace0a18182a473a3671386d
Samuel Weirich <4281791+SamuelWei@users.noreply.github.com>
2026-02-12 11:28:14 +0100

    Fix config file

--- tests/Visual/happo.config.js
+++ tests/Visual/happo.config.js
@@ -3,7 +3,9 @@ import { defineConfig } from "happo";
 export default defineConfig({
   apiKey: process.env.HAPPO_API_KEY,
   apiSecret: process.env.HAPPO_API_SECRET,
-
+  integration: {
+    type: "cypress",
+  },
   targets: {
     "chrome-desktop": {
       type: "chrome",
