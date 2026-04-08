374c7778bef34b924cacddf1882bcd3eb0164d60
Samuel Weirich <4281791+SamuelWei@users.noreply.github.com>
2026-04-08 21:06:10 +0200

    Fix type bug in test room int access code

--- tests/Backend/Feature/api/v1/Room/RoomTest.php
+++ tests/Backend/Feature/api/v1/Room/RoomTest.php
@@ -737,7 +737,7 @@ class RoomTest extends TestCase
     {
         $room = Room::factory()->create([
             'allow_guests' => true,
-            'access_code' => 111111111,
+            'access_code' => '111111111',
         ]);
 
         // Try 6 times with wrong access code
