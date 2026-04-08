beb29f0cc2faff3ab313b859cba3318b7261e919
Samuel Weirich <4281791+SamuelWei@users.noreply.github.com>
2026-04-08 21:06:32 +0200

    Fix broken attestation

--- tests/Backend/Unit/Console/PollServerTest.php
+++ tests/Backend/Unit/Console/PollServerTest.php
@@ -129,10 +129,10 @@ class PollServerTest extends TestCase
 
         // Check server archival data
         $this->assertEquals(1, $meeting->server->stats()->count());
-        $this->assertNotNull(6, $meeting->server->stats->last()->participant_count);
-        $this->assertNotNull(3, $meeting->server->stats->last()->listener_count);
-        $this->assertNotNull(3, $meeting->server->stats->last()->voice_participant_count);
-        $this->assertNotNull(2, $meeting->server->stats->last()->video_count);
+        $this->assertEquals(6, $meeting->server->stats->last()->participant_count);
+        $this->assertEquals(3, $meeting->server->stats->last()->listener_count);
+        $this->assertEquals(3, $meeting->server->stats->last()->voice_participant_count);
+        $this->assertEquals(2, $meeting->server->stats->last()->video_count);
         $this->assertEquals(1, $meeting->server->stats->last()->meeting_count);
 
         // Check server live data
