4c395ee06986e61f0d8a9c3e10d6c7fd83994d36
SamuelWei <4281791+SamuelWei@users.noreply.github.com>
2026-04-09 11:22:29 +0000

    Update locales

--- lang/de/app.php
+++ lang/de/app.php
@@ -64,7 +64,6 @@ return [
     'flash' => [
         'client_error' => 'Es ist ein unbekannter Fehler in der Anwendung aufgetreten!',
         'guests_only' => 'Die Anfrage ist nur für nicht angemeldete Benutzer gestattet!',
-        'popup_blocked' => 'Ihr Browser hat das Öffnen eines neuen Fensters blockiert. Bitte erlauben Sie das Öffnen von Popups für diese Seite und versuchen Sie es erneut.',
         'server_error' => [
             'empty_message' => 'Es ist ein Fehler auf dem Server aufgetreten!',
             'error_code' => 'Fehlercode: :statusCode',
