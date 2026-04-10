3d1b5a6c3fc0a27dcbe8fadfe31eca2f18a37904
Samuel Weirich <4281791+SamuelWei@users.noreply.github.com>
2026-04-10 10:26:13 +0200

    Add scroll to error functionality

--- resources/js/components/FormError.vue
+++ resources/js/components/FormError.vue
@@ -9,7 +9,11 @@ const props = defineProps({
 </script>
 
 <template>
-  <p class="text-red-500" role="alert">
+  <p
+    v-if="props.errors != null && Object.keys(props.errors).length > 0"
+    class="form-error text-red-500"
+    role="alert"
+  >
     <template v-for="(error, index) in props.errors" :key="index">
       {{ error }}
       <br v-if="index < props.errors.length - 1" />
