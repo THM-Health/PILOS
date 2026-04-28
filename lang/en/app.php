506331ff8de5256e59f57f4d36951e105da4547f
Sabrina Wüst <119059706+Sabr1n4W@users.noreply.github.com>
2026-04-28 11:36:07 +0200

    Update locales
    Co-authored-by: Samuel Weirich <4281791+samuelwei@users.noreply.github.com>

--- lang/en/app.php
+++ lang/en/app.php
@@ -68,7 +68,7 @@ return [
         'guests_only' => 'The request can only be made by guests!',
         'model_not_found' => [
             'title' => 'The :model was not found!',
-            'details' => 'Ids: :ids',
+            'details' => 'IDs: :ids',
         ],
         'server_error' => [
             'empty_message' => 'An error occurred on the server during request!',
