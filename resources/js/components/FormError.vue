d104d41fbbe03be511056a26cf7dd15cf316314f
Samuel Weirich <4281791+SamuelWei@users.noreply.github.com>
2026-05-20 14:54:02 +0200

    Move viewport check to utils

--- resources/js/components/FormError.vue
+++ resources/js/components/FormError.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { computed, nextTick, onMounted, ref, watch } from "vue";
+import { isElementInViewport } from "../utils/viewport";
 
 const props = defineProps({
   errors: {
@@ -15,23 +16,7 @@ const hasError = computed(() => {
   return props.errors != null && Object.keys(props.errors).length > 0;
 });
 
-function isElementInViewport(el) {
-  var rect = el.getBoundingClientRect();
-
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <=
-      (window.innerHeight ||
-        document.documentElement.clientHeight) /*or $(window).height() */ &&
-    rect.right <=
-      (window.innerWidth ||
-        document.documentElement.clientWidth) /*or $(window).width() */
-  );
-}
-
 async function scrollToFirstError() {
-  await nextTick();
   await nextTick();
 
   const selfDomElement = formError.value;
---
+++ resources/js/utils/viewport.js
@@ -0,0 +1,11 @@
+export function isElementInViewport(el) {
+  const rect = el.getBoundingClientRect();
+
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
