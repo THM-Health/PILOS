6b86e82f31840a6150c4099fb0f8c6bc6c67ce33
Samuel Weirich <4281791+SamuelWei@users.noreply.github.com>
2026-07-17 16:25:02 +0200

    Refactor popover, add more instructions, dark mode and a11y

--- resources/js/components/RoomJoinByPhoneButton.vue
+++ resources/js/components/RoomJoinByPhoneButton.vue
@@ -4,72 +4,106 @@
     :label="$t('rooms.phone.join_by_phone')"
     icon="fa-solid fa-phone"
     severity="secondary"
-    @click="toggle"
     class="flex-shrink-0"
+    @click="toggle"
   />
-  <OverlayPanel ref="op" aria-labelledby="room-invitation-title">
-    <div class="align-items-start flex min-w-min flex-col gap-3 p-2">
-      <fieldset class="flex w-full flex-col gap-2">
-        <legend
-          id="room-invitation-title"
-          class="white-space-nowrap block font-bold"
-        >
-          {{ $t("rooms.phone.title") }}
-        </legend>
-        <div class="flex-grow-1">
-          <IconField iconPosition="left">
-            <InputIcon>
+  <OverlayPanel ref="op" aria-labelledby="room-join-by-phone-title">
+    <div class="flex w-min flex-col">
+      <div class="flex flex-col items-start gap-4 p-2">
+        <div class="flex justify-between gap-4">
+          <h2
+            id="room-join-by-phone-title"
+            tabindex="-1"
+            autofocus
+            class="block font-bold whitespace-nowrap"
+          >
+            {{ $t("rooms.phone.title") }}
+          </h2>
+          <Button
+            class="popover-close-button"
+            data-test="popover-close-button"
+            :aria-label="$t('app.close')"
+            text
+            rounded
+            severity="secondary"
+            icon="fas fa-xmark"
+            @click="closePopover"
+          />
+        </div>
+      </div>
+      <div class="grow">
+        <div class="mt-2 flex flex-row items-center">
+          <InputGroup>
+            <InputGroupAddon
+              class="min-w-5 border-none px-0 dark:bg-surface-900"
+              aria-hidden="true"
+            >
               <i
-                class="fa-solid fa-phone"
                 v-tooltip="$t('rooms.phone.number')"
+                class="fa-solid fa-phone"
               />
-            </InputIcon>
+            </InputGroupAddon>
             <InputText
-              class="text-overflow-ellipsis w-full border-0 shadow-none"
-              id="invitationLink"
+              id="phone-number"
+              class="w-min border-none text-ellipsis shadow-none dark:bg-surface-900"
               :aria-label="$t('rooms.phone.number')"
               readonly
               :value="number"
+              @focus="$event.target.select()"
             />
-          </IconField>
+          </InputGroup>
+        </div>
 
-          <IconField iconPosition="left">
-            <InputIcon>
-              <i class="fa-solid fa-lock" v-tooltip="$t('rooms.phone.pin')" />
-            </InputIcon>
+        <div class="mt-2 flex flex-row items-center">
+          <InputGroup>
+            <InputGroupAddon
+              class="min-w-5 border-none px-0 dark:bg-surface-900"
+              aria-hidden="true"
+            >
+              <i v-tooltip="$t('rooms.phone.pin')" class="fa-solid fa-lock" />
+            </InputGroupAddon>
             <InputText
-              class="w-full border-0 shadow-none"
-              id="invitationCode"
+              id="phone-pin"
+              class="w-full border-none shadow-none dark:bg-surface-900"
               :aria-label="$t('rooms.phone.pin')"
               readonly
               :value="pin"
+              @focus="$event.target.select()"
             />
-          </IconField>
+          </InputGroup>
         </div>
-      </fieldset>
+      </div>
+      <divider />
 
-      <div class="flex justify-center">
+      <div class="flex flex-col items-center">
         <img :src="qrcode" :alt="$t('rooms.phone.qrcode')" />
+        <p class="text-center">
+          {{ $t("rooms.phone.scan_qr_code") }}
+        </p>
       </div>
 
       <Button
+        data-test="join-by-phone-call-button"
         as="a"
         :href="link"
-        class="p-button p-button-secondary"
         icon="fa-solid fa-phone"
+        class="mt-4 w-full"
         :label="$t('rooms.phone.call')"
       />
     </div>
   </OverlayPanel>
 </template>
 <script setup>
-import { computed, ref } from "vue";
-import { useQRCode } from "@vueuse/integrations/useQRCode";
+import { computed, nextTick, onMounted, ref, watch } from "vue";
+import { useDark } from "@vueuse/core";
+import QRCode from "qrcode";
 
+const isDark = useDark();
 const op = ref();
-const toggle = (event) => {
-  op.value.toggle(event);
-};
+const triggerButton = ref(null);
+const textColor = ref("#000");
+const backgroundColor = ref("#FFF");
+const qrcode = ref();
 
 const props = defineProps({
   number: {
@@ -82,10 +116,49 @@ const props = defineProps({
   },
 });
 
+onMounted(() => {
+  setColors();
+});
+
+const toggle = (event) => {
+  triggerButton.value = event.currentTarget;
+  op.value.toggle(event);
+};
+
+function closePopover() {
+  op.value.hide();
+  if (triggerButton.value != null) {
+    triggerButton.value.focus();
+  }
+}
+
 const link = computed(() => {
   // Remove all chars that are not digits or '+'
   const cleanNumber = props.number.replace(/[^0-9+]/g, "");
   return `tel:${cleanNumber},${props.pin}#`;
 });
-const qrcode = useQRCode(link);
+
+function setColors() {
+  const style = getComputedStyle(document.body);
+  textColor.value = style.getPropertyValue("--p-text-color");
+  backgroundColor.value = style.getPropertyValue("--p-popover-background");
+}
+
+watch(
+  [link, textColor, backgroundColor],
+  async () => {
+    qrcode.value = await QRCode.toDataURL(link.value, {
+      color: {
+        dark: textColor.value,
+        light: backgroundColor.value,
+      },
+    });
+  },
+  { immediate: true },
+);
+
+watch(isDark, async () => {
+  await nextTick();
+  setColors();
+});
 </script>
