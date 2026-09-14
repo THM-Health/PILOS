057e206ed2e6c734e248ce7c603876b29af10a43
Samuel Weirich <4281791+SamuelWei@users.noreply.github.com>
2026-09-14 18:31:26 +0200

    Cleanup

--- tests/System/cypress.config.js
+++ tests/System/cypress.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
   allowCypressEnv: false,
 
   e2e: {
-    async setupNodeEvents(on, config) {
+    async setupNodeEvents(config) {
       const chromeForTesting = await findChromeForTesting();
 
       if (chromeForTesting.length > 0) {
