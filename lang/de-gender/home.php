3788946b3c9c2c7b8e73c080503aceb34dc3ec9c
SamuelWei <4281791+SamuelWei@users.noreply.github.com>
2026-09-15 14:09:55 +0000

    Update locales

--- lang/de-gender/home.php
+++ lang/de-gender/home.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 return [
-    'description_details' => 'PILOS wurde speziell für den Bildungsbereich entwickelt und bietet eine unkomplizierte, interaktive und sichere Lösung für das Online-Lernen, den Unterricht und alle Arten von virtuellen Meetings. PILOS lässt sich an die jeweiligen Bedürfnisse anpassen und bietet hochwertige Videoübertragung, robuste Tools für die Zusammenarbeit und eine intuitive Benutzeroberfläche.',
+    'description_details' => 'PILOS wurde speziell für den Bildungsbereich entwickelt und bietet eine unkomplizierte, interaktive und sichere Lösung für das Online-Lernen, den Unterricht und alle Arten von virtuellen Meetings. PILOS lässt sich an die jeweiligen Bedürfnisse anpassen und bietet hochwertige Videoübertragung, robuste Tools für die Zusammenarbeit und eine intuitive Oberfläche.',
     'description_intro' => 'Die universelle Plattform für Videokonferenzen, basierend auf BigBlueButton™.',
     'features' => [
         'collaboration' => [
