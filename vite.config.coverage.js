3e492f769743b75d80260b6d56c8d69c35254896
Samuel Weirich <4281791+SamuelWei@users.noreply.github.com>
2026-09-23 14:56:33 +0200

    Bump vite to v8

--- vite.config.coverage.js
+++ vite.config.coverage.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import commonConfig from "./vite.config.common";
+import commonConfig from "./vite.config.common.js";
 import istanbul from "vite-plugin-istanbul";
 export default ({ mode }) => {
   const config = commonConfig(mode);
