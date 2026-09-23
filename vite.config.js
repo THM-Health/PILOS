c22f9fcce01de5e1c750c3905364972def251d96
Samuel Weirich <4281791+samuelwei@users.noreply.github.com>
2026-09-23 15:32:27 +0200

    Bump vite to v8 (#3479)

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import commonConfig from "./vite.config.common";
+import commonConfig from "./vite.config.common.js";
 export default ({ mode }) => {
   return defineConfig(commonConfig(mode));
 };
